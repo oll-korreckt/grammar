937ba0f17358432d7f8b6027235e6951afa4f682
ok <oll.korreckt@gmail.com>
2021-05-03 17:30:49 -0400

    Made modules for handling simple objects.

---
+++ src/utils/__tests__/simple-object.tests.ts
@@ -0,0 +1,110 @@
+import { assert } from "chai";
+import { SimpleObject } from "..";
+
+describe("deepEquals", () => {
+    describe("typed", () => {
+        test("diff prop values", () => {
+            const obj1 = {
+                name: "bob",
+                age: 22
+            };
+            const obj2 = {
+                name: "bob",
+                age: 23
+            };
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isFalse(result);
+        });
+
+        test("diff shape", () => {
+            const obj1 = {
+                firstName: "bob",
+                lastName: "smith"
+            };
+            const obj2 = {
+                firstName: "bob",
+                lastName: "smith",
+                age: 22
+            };
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isFalse(result);
+        });
+
+        test("diff nested shape", () => {
+            const obj1 = {
+                A: true,
+                B: {
+                    b1: 1,
+                    b2: 2,
+                    b3: 3
+                }
+            };
+            const obj2 = {
+                A: true,
+                B: {
+                    b1: 1,
+                    b2: 2,
+                    b3: 3,
+                    b4: 4
+                }
+            };
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isFalse(result);
+        });
+
+        test("diff nested prop values", () => {
+            const obj1 = {
+                A: true,
+                B: {
+                    b1: true
+                }
+            };
+            const obj2 = {
+                A: true,
+                B: {
+                    b1: false
+                }
+            };
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isFalse(result);
+        });
+
+        test("equal", () => {
+            const obj1 = {
+                name: "value",
+                obj: {
+                    name: "value"
+                }
+            };
+            const obj2 = {
+                name: "value",
+                obj: {
+                    name: "value"
+                }
+            };
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isTrue(result);
+        });
+    });
+
+    describe("untyped", () => {
+        test("diff prop names", () => {
+            const obj1 = { name: "bob" } as any;
+            const obj2 = { age: 22 } as any;
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isFalse(result);
+        });
+
+        test("diff prop types", () => {
+            const obj1 = { name: "bob" } as any;
+            const obj2 = { name: 5 } as any;
+            const result = SimpleObject.deepEquals(obj1, obj2);
+            assert.isFalse(result);
+        });
+
+        test("not objects", () => {
+            const result = SimpleObject.deepEquals("string" as any, 2 as any);
+            assert.isFalse(result);
+        });
+    });
+});
---
+++ src/utils/simple-object.ts
@@ -0,0 +1,59 @@
+export type SimpleObject = {
+    [key: string]:
+        | undefined
+        | boolean
+        | number
+        | bigint
+        | string
+        | SimpleObject;
+};
+
+function deepEquals<T extends SimpleObject>(obj1: T, obj2: T): boolean {
+    function getEntries(obj: T) {
+        return Object.entries(obj)
+            .filter(([, value]) => {
+                const valueType = typeof value;
+                return valueType !== "undefined";
+            })
+            .sort(([key1], [key2]) => {
+                if (key1 < key2) {
+                    return -1;
+                }
+                if (key1 > key2) {
+                    return 1;
+                }
+                return 0;
+            });
+    }
+    const obj1Entries = getEntries(obj1);
+    const obj2Entries = getEntries(obj2);
+    if (obj1Entries.length !== obj2Entries.length) {
+        return false;
+    }
+    for (let i = 0; i < obj1Entries.length; i++) {
+        const [key1, value1] = obj1Entries[i];
+        const [key2, value2] = obj2Entries[i];
+        if (key1 !== key2) {
+            return false;
+        }
+        const type1 = typeof value1;
+        const type2 = typeof value2;
+        if (type1 !== type2) {
+            return false;
+        }
+        if (type1 === "object") {
+            if (!deepEquals(value1 as SimpleObject, value2 as SimpleObject)) {
+                return false;
+            }
+        } else {
+            if (value1 !== value2) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+export const SimpleObject = {
+    deepEquals: deepEquals
+};
