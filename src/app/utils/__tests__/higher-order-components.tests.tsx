77b7bbac936c15d59065c9551460c8af8a41069a
ok <oll.korreckt@gmail.com>
2021-07-12 10:29:54 -0400

    Revised the core HOCs to use refs instead of wrapping components in divs

--- src/app/utils/__tests__/higher-order-components.tests.tsx
+++ src/app/utils/__tests__/higher-order-components.tests.tsx
@@ -1,18 +1,48 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import { withClassContainer } from "..";
+import { fireEvent, render } from "@testing-library/react";
+import { withClassName, makeRefComponent, withOnClick } from "../higher-order-components";
 
-const MockComponent: React.VFC<{ children: string }> = ({ children }) => {
-    return <span>{children}</span>;
-};
+const MockComponent = makeRefComponent<HTMLDivElement, { children: string; }>("MockComponent", ({ children }, ref) => {
+    return <div ref={ref}>{children}</div>;
+});
 
-describe("withClassContainer", () => {
-    test("works", () => {
-        const NewComponent = withClassContainer(MockComponent, "hello");
-        const result = render(<NewComponent>Some text</NewComponent>);
-        const child = result.getByText("Some text");
-        expect(child).toBeDefined();
-        const container = result.container.firstChild as HTMLDivElement;
-        expect(container.className).toEqual("hello");
+describe("higher-order-components", () => {
+    describe("withOnClick", () => {
+        test("with 1", () => {
+            const callback = jest.fn(() => { return; });
+            const WrappedComponent = withOnClick(MockComponent, callback);
+            const result = render(<WrappedComponent>Some text</WrappedComponent>).getByText("Some text");
+            fireEvent(result, new MouseEvent("click"));
+            expect(callback).toBeCalledTimes(1);
+        });
+
+        test("with 2", () => {
+            const callback1 = jest.fn(() => { return; });
+            const callback2 = jest.fn(() => { return; });
+            const WrappedComponent = withOnClick(withOnClick(MockComponent, callback1), callback2);
+            const result = render(<WrappedComponent>Some text</WrappedComponent>).getByText("Some text");
+            fireEvent(result, new MouseEvent("click"));
+            expect(callback1).toBeCalledTimes(1);
+            expect(callback2).toBeCalledTimes(1);
+        });
+    });
+
+    describe("withClassName", () => {
+        test("with 1", () => {
+            const WrappedComponent = withClassName(MockComponent, "hello");
+            const result = render(<WrappedComponent>Some text</WrappedComponent>).getByText("Some text");
+            expect(result.className).toBe("hello");
+        });
+
+        test("with 2", () => {
+            const WrappedComponent = withClassName(withClassName(MockComponent, "hello2"), "hello1");
+            const result = render(<WrappedComponent>Some text</WrappedComponent>).getByText("Some text");
+            const classNames: string[] = [];
+            result.classList.forEach((value) => classNames.push(value));
+            classNames.sort();
+            const expected = ["hello1", "hello2"].sort();
+
+            expect(classNames).toStrictEqual(expected);
+        });
     });
 });
--- src/app/utils/higher-order-components.tsx
+++ src/app/utils/higher-order-components.tsx
@@ -1,15 +1,50 @@
 import React from "react";
 
-export function withClassContainer<T>(Component: React.VFC<T>, className: string): React.VFC<T>;
-export function withClassContainer<T>(Component: React.FC<T>, className: string): React.FC<T>;
-export function withClassContainer<T>(Component: React.VFC<T> | React.FC<T>, className: string): React.VFC<T> | React.FC<T> {
-    const output = (props: T) => {
-        return (
-            <div className={className}>
-                <Component {...props} />
-            </div>
-        );
-    };
-    output.displayName = Component.displayName;
+export type RefComponent<TElement extends HTMLElement, TProps> = React.ForwardRefExoticComponent<React.PropsWithoutRef<TProps> & React.RefAttributes<TElement>>;
+type Action<TElement extends HTMLElement> = (instance: TElement) => void;
+
+export function makeRefComponent<TElement extends HTMLElement, TProps>(displayName: string, render: React.ForwardRefRenderFunction<TElement, TProps>): RefComponent<TElement, TProps> {
+    const output = React.forwardRef(render);
+    output.displayName = displayName;
     return output;
+}
+
+export function makeRefHoc<TElement extends HTMLElement, TProps>(
+    Component: RefComponent<TElement, TProps>,
+    wrapperName: string,
+    action: Action<TElement>): RefComponent<TElement, TProps> {
+    const hocRenderFunction: React.ForwardRefRenderFunction<TElement, TProps> = (props, ref) => {
+        let newRef: React.Ref<TElement>;
+        if (ref === null) {
+            newRef = (i) => i && action(i);
+        } else if (typeof ref === "object") {
+            newRef = (i) => {
+                ref.current = i;
+                i && action(i);
+            };
+        } else if (typeof ref === "function") {
+            newRef = (i) => {
+                ref(i);
+                i && action(i);
+            };
+        } else {
+            throw "Unexpected ref. Received a value that is not null, an object, or a function.";
+        }
+        return <Component {...props} ref={newRef} />;
+    };
+    const output = React.forwardRef(hocRenderFunction);
+    output.displayName = `${wrapperName}(${Component.displayName})`;
+    return output;
+}
+
+export function withOnClick<TElement extends HTMLElement, TProps>(
+    Component: RefComponent<TElement, TProps>,
+    onClick: () => void): RefComponent<TElement, TProps> {
+    return makeRefHoc(Component, "withOnClick", (instance) => instance.addEventListener("click", onClick));
+}
+
+export function withClassName<TElement extends HTMLElement, TProps>(
+    Component: RefComponent<TElement, TProps>,
+    className: string): RefComponent<TElement, TProps> {
+    return makeRefHoc(Component, "withClassName", (instance) => instance.classList.add(className));
 }
