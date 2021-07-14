95889c04f5dc7148ed4b4fe9194f92b83f0e75a5
ok <oll.korreckt@gmail.com>
2021-07-14 14:15:05 -0400

    Added exports for new components

--- src/app/basic-components/index.ts
+++ src/app/basic-components/index.ts
@@ -2,3 +2,6 @@ export * from "./XToggleButton/XToggleButton";
 export { XToggleRadioButton } from "./XToggleRadioButton/XToggleRadioButton";
 export type { XToggleRadioButtonProps, XToggleRadioButtonChild } from "./XToggleRadioButton/XToggleRadioButton";
 export * from "./Buttons/Buttons";
+export * from "./ButtonBar";
+export * from "./ElementFilter";
+export * from "./ElementLabel";
