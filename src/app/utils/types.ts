3a948a758bc6f446a0061ab8a6d851642bc9791d
ok <oll.korreckt@gmail.com>
2021-07-23 13:27:29 -0400

    Created SimpleComponent type

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -1,1 +1,5 @@
+import React from "react";
+
 export type ElementFilterType = "word" | "partOfSpeech" | "phrase" | "clause" | "sentence";
+export type SimpleComponentProps = { children: string; }
+export type SimpleComponent = React.FC<SimpleComponentProps>;
