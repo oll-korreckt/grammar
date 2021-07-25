c0da08b140b2c8be01b619b3c3c2607bb89b4874
ok <oll.korreckt@gmail.com>
2021-07-25 15:35:16 -0400

    Refactored to use ElementDisplayInfo

--- src/app/tricky-components/EditDiagram/EditDiagram.tsx
+++ src/app/tricky-components/EditDiagram/EditDiagram.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
-import { Colors, DiagramState, DiagramStateContext, DisplayModelContext, withOnClick, accessClassName, ElementSelectNode, SimpleComponent, DiagramStateItem, DisplayModel, makeRefComponent, useOutsideClick, SimpleComponentProps, SelectedElement, isHeadElementSelectNode, isTailElementSelectNode } from "@app/utils";
+import { DiagramState, DiagramStateContext, DisplayModelContext, withOnClick, accessClassName, ElementSelectNode, SimpleComponent, DiagramStateItem, DisplayModel, makeRefComponent, useOutsideClick, SimpleComponentProps, SelectedElement, isHeadElementSelectNode, isTailElementSelectNode, ElementDisplayInfo } from "@app/utils";
 import { ElementLabel, withHeadLabel, withSideBorder } from "@app/basic-components";
-import { ElementReference, ElementType, getElementDefinition } from "@domain/language";
+import { ElementReference, getElementDefinition } from "@domain/language";
 import styles from "./_styles.scss";
 
 type LabelClickFunction = (node: ElementSelectNode) => void;
@@ -76,8 +76,8 @@ function addItemLabels(model: DisplayModel, node: ElementSelectNode, labelClick:
     const output: (SimpleComponent | undefined)[] = model.word.map(() => undefined);
     const positions = model.elements[node.id].words;
     positions.forEach((position, index) => {
-        const { headLabel, color } = getDisplayOptions(node.type);
-        let Label = generateLabelBorder(headLabel, index, positions);
+        const { header, color } = ElementDisplayInfo.getDisplayInfo(node.type);
+        let Label = generateLabelBorder(header, index, positions);
         Label = withOnClick(Label, () => labelClick(node));
         const fnCmp: SimpleComponent = ({ children }) => <Label color={color}>{children}</Label>;
         fnCmp.displayName = Label.displayName;
@@ -216,169 +216,3 @@ export const EditDiagram: React.VFC = () => {
         </div>
     );
 };
-
-function getDisplayOptions(type: ElementType): { headLabel: string; color: Colors; } {
-    type OutputObj = { headLabel: string; color: Colors; }
-    type SwitchObj = {
-        [Key in ElementType]: OutputObj;
-    }
-    const word: OutputObj = {
-        color: "color10",
-        headLabel: "word"
-    };
-    const nounColor: Colors = "color1";
-    const noun: OutputObj = {
-        color: nounColor,
-        headLabel: "noun"
-    };
-    const nounPhrase: OutputObj = {
-        color: nounColor,
-        headLabel: "NP"
-    };
-    const nounClause: OutputObj = {
-        color: nounColor,
-        headLabel: "NC"
-    };
-    const pronoun: OutputObj = {
-        color: "color2",
-        headLabel: "pro."
-    };
-    const verbColor: Colors = "color3";
-    const verb: OutputObj = {
-        color: verbColor,
-        headLabel: "verb"
-    };
-    const verbPhrase: OutputObj = {
-        color: verbColor,
-        headLabel: "VP"
-    };
-    const infColor: Colors = "color4";
-    const inf: OutputObj = {
-        color: infColor,
-        headLabel: "inf."
-    };
-    const infPhrase: OutputObj = {
-        color: infColor,
-        headLabel: "IP"
-    };
-    const partColor: Colors = "color5";
-    const part: OutputObj = {
-        color: partColor,
-        headLabel: "part."
-    };
-    const partPhrase: OutputObj = {
-        color: partColor,
-        headLabel: "PP"
-    };
-    const gerColor: Colors = "color6";
-    const ger: OutputObj = {
-        color: gerColor,
-        headLabel: "ger."
-    };
-    const gerPhrase: OutputObj = {
-        color: gerColor,
-        headLabel: "GP"
-    };
-    const adjColor: Colors = "color7";
-    const adj: OutputObj = {
-        color: adjColor,
-        headLabel: "adj."
-    };
-    const adjPhrase: OutputObj = {
-        color: adjColor,
-        headLabel: "AP"
-    };
-    const advColor: Colors = "color8";
-    const adv: OutputObj = {
-        color: advColor,
-        headLabel: "adv."
-    };
-    const advPhrase: OutputObj = {
-        color: advColor,
-        headLabel: "AP"
-    };
-    const advClause: OutputObj = {
-        color: advColor,
-        headLabel: "AC"
-    };
-    const prepColor: Colors = "color9";
-    const prep: OutputObj = {
-        color: prepColor,
-        headLabel: "prep."
-    };
-    const prepPhrase: OutputObj = {
-        color: prepColor,
-        headLabel: "PP"
-    };
-    const detColor: Colors = "color10";
-    const det: OutputObj = {
-        color: detColor,
-        headLabel: "det."
-    };
-    const coord: OutputObj = {
-        color: "color11",
-        headLabel: "coord."
-    };
-    const sub: OutputObj = {
-        color: "color12",
-        headLabel: "sub."
-    };
-    const relClause: OutputObj = {
-        color: adjColor,
-        headLabel: "RC"
-    };
-    const indClause: OutputObj = {
-        color: "color11",
-        headLabel: "IC"
-    };
-    const switchObj: SwitchObj = {
-        word: word,
-        noun: noun,
-        coordinatedNoun: noun,
-        nounPhrase: nounPhrase,
-        coordinatedNounPhrase: nounPhrase,
-        nounClause: nounClause,
-        coordinatedNounClause: nounClause,
-        pronoun: pronoun,
-        coordinatedPronoun: pronoun,
-        verb: verb,
-        coordinatedVerb: verb,
-        verbPhrase: verbPhrase,
-        coordinatedVerbPhrase: verbPhrase,
-        infinitive: inf,
-        coordinatedInfinitive: inf,
-        infinitivePhrase: infPhrase,
-        coordinatedInfinitivePhrase: infPhrase,
-        participle: part,
-        coordinatedParticiple: part,
-        participlePhrase: partPhrase,
-        coordinatedParticiplePhrase: partPhrase,
-        gerund: ger,
-        coordinatedGerund: ger,
-        gerundPhrase: gerPhrase,
-        coordinatedGerundPhrase: gerPhrase,
-        adjective: adj,
-        coordinatedAdjective: adj,
-        adjectivePhrase: adjPhrase,
-        coordinatedAdjectivePhrase: adjPhrase,
-        adverb: adv,
-        coordinatedAdverb: adv,
-        adverbPhrase: advPhrase,
-        coordinatedAdverbPhrase: advPhrase,
-        adverbialClause: advClause,
-        coordinatedAdverbialClause: advClause,
-        preposition: prep,
-        coordinatedPreposition: prep,
-        prepositionPhrase: prepPhrase,
-        coordinatedPrepositionPhrase: prepPhrase,
-        determiner: det,
-        coordinatedDeterminer: det,
-        subordinator: sub,
-        coordinator: coord,
-        relativeClause: relClause,
-        coordinatedRelativeClause: relClause,
-        independentClause: indClause,
-        coordinatedIndependentClause: indClause
-    };
-    return switchObj[type];
-}
