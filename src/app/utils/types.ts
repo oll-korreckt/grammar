4fcab8de8b6febf37ef9ef2d1c72726aaaf9971b
ok <oll.korreckt@gmail.com>
2021-07-29 16:45:00 -0400

    Created a Rect type to represent client rectangles

--- src/app/utils/types.ts
+++ src/app/utils/types.ts
@@ -3,3 +3,22 @@ import React from "react";
 export type ElementFilterType = "word" | "partOfSpeech" | "phrase" | "clause" | "sentence";
 export type SimpleComponentProps = { children: string; }
 export type SimpleComponent = React.FC<SimpleComponentProps>;
+export type Rect = {
+    top: number;
+    left: number;
+    width: number;
+    height: number;
+}
+
+function init(domRect: DOMRect): Rect {
+    return {
+        top: domRect.top,
+        left: domRect.left,
+        width: domRect.width,
+        height: domRect.height
+    };
+}
+
+export const Rect = {
+    init: init
+};
