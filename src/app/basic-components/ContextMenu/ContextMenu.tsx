eed09a2e2d1a9ea5ce12898ec7ce13ae2951cbfb
ok <oll.korreckt@gmail.com>
2021-07-30 11:29:01 -0400

    Created a simple ContextMenu component

---
+++ src/app/basic-components/ContextMenu/ContextMenu.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { accessClassName, useOutsideClick } from "@app/utils";
+import styles from "./_styles.scss";
+
+export interface ContextMenuProps {
+    children: [string, ...string[]];
+    onItemSelect?: (item: string) => void;
+    onCancel?: () => void;
+}
+
+function isUnique(items: string[]): boolean {
+    const set = new Set(items);
+    return set.size === items.length;
+}
+
+export const ContextMenu: React.VFC<ContextMenuProps> = ({ children, onItemSelect, onCancel }) => {
+    if (!isUnique(children)) {
+        throw "Items must be unique";
+    }
+    const ref = useOutsideClick<HTMLDivElement>(() => onCancel && onCancel());
+    return (
+        <div ref={ref} className={accessClassName(styles, "contextMenu")}>
+            {children.map((child) => {
+                return (
+                    <div
+                        className={accessClassName(styles, "item")}
+                        onClick={() => onItemSelect && onItemSelect(child)}
+                        key={child}
+                    >
+                        <div>{child}</div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
