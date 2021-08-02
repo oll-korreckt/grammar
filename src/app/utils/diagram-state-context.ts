a2eda06ee19dffb18bae07a1b7e53ffec6b814cb
ok <oll.korreckt@gmail.com>
2021-08-02 14:33:26 -0400

    Added DisplayModel to DiagramStateContext

--- src/app/utils/diagram-state-context.ts
+++ src/app/utils/diagram-state-context.ts
@@ -1,16 +1,21 @@
 import { createContext, Dispatch, SetStateAction } from "react";
 import { DiagramState } from "./diagram-state";
+import { DisplayModel } from "./display-model";
 import { HistoryState } from "./history-state";
 
 export interface DiagramStateContext {
     state: HistoryState<DiagramState>;
     setState: Dispatch<SetStateAction<HistoryState<DiagramState>>>;
+    model: DisplayModel;
 }
 
+const state = HistoryState.init<DiagramState>({
+    wordOrder: [],
+    elements: {}
+});
+const model = DisplayModel.init(state.currState);
 export const DiagramStateContext = createContext<DiagramStateContext>({
-    state: HistoryState.init({
-        elements: {},
-        wordOrder: []
-    }),
+    state: state,
+    model: model,
     setState: () => { throw "not implemented"; }
 });
