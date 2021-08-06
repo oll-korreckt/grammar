3b8b0edd078e01e648f811f53f06eb5e18e669e6
ok <oll.korreckt@gmail.com>
2021-08-06 16:36:12 -0400

    Created simplified way of putting words on diagram.

---
+++ src/app/basic-components/Word/Word.tsx
@@ -0,0 +1,33 @@
+import { accessClassName, Colors } from "@app/utils";
+import { makeRefComponent } from "@app/utils/hoc";
+import React, { ReactNode } from "react";
+import styles from "./_styles.scss";
+
+export const Space: React.VFC = () => {
+    return <span className={accessClassName(styles, "word")}>&#32;</span>;
+};
+
+export interface WordProps {
+    children: ReactNode;
+}
+
+export const Word = makeRefComponent<HTMLSpanElement, WordProps>("Word", ({ children }, ref) => {
+    return <span className={accessClassName(styles, "word")} ref={ref}>{children}</span>;
+});
+
+export interface HeadLabelProps {
+    children: string;
+}
+
+export const HeadLabel = makeRefComponent<HTMLDivElement, HeadLabelProps>("HeadLabel", ({ children }, ref) => {
+    return <div className={accessClassName(styles, "headLabel")} ref={ref}>{children}</div>;
+});
+
+export interface WordLabelProps {
+    children: ReactNode;
+    color: Colors;
+}
+
+export const WordLabel = makeRefComponent<HTMLSpanElement, WordLabelProps>("WordLabel", ({ color, children }, ref) => {
+    return <span className={accessClassName(styles, "wordLabel", color)} ref={ref}>{children}</span>;
+});
