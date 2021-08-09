023a1c6fc817b592470f8337f9296793542e781f
ok <oll.korreckt@gmail.com>
2021-08-09 15:59:57 -0400

    Revised EditDiagram

--- src/app/tricky-components/EditDiagram/EditDiagram.tsx
+++ src/app/tricky-components/EditDiagram/EditDiagram.tsx
@@ -1,218 +1,220 @@
 import React, { useContext } from "react";
-import { DiagramState, DiagramStateContext, DisplayModelContext, withOnClick, accessClassName, ElementSelectNode, SimpleComponent, DiagramStateItem, DisplayModel, makeRefComponent, useOutsideClick, SimpleComponentProps, SelectedElement, isHeadElementSelectNode, isTailElementSelectNode, ElementDisplayInfo } from "@app/utils";
-import { ElementLabel, withHeadLabel, withSideBorder } from "@app/basic-components";
-import { ElementReference, getElementDefinition } from "@domain/language";
-import styles from "./_styles.scss";
+import { DiagramState, DiagramStateContext, DisplayModel, DisplayModelContext, ElementCategory, ElementDisplayInfo, ElementSelectState, SelectedElement, WordIndices, WordRange } from "@app/utils";
+import { ElementId, ElementType } from "@domain/language";
+import { HeadLabel, Space, WordLabel, Word } from "@app/basic-components/Word";
+import { makeRefComponent, RefComponent } from "@app/utils/hoc";
 
-type LabelClickFunction = (node: ElementSelectNode) => void;
-type NakedWordClickFunction = () => void;
-
-const NEITHER_PRESENT = 0;
-const LEFT_PRESENT = 1 << 0;
-const RIGHT_PRESENT = 1 << 1;
-const BOTH_PRESENT = LEFT_PRESENT | RIGHT_PRESENT;
-
-function getLexeme(diagram: DiagramState, index: number): string {
-    const id = diagram.wordOrder[index];
-    return DiagramState.getTypedItem(diagram, "word", id).value.lexeme;
+function getLexemes(diagram: DiagramState, index: WordIndices[number]): string[] {
+    return (Array.isArray(index) ? WordRange.expand(index) : [index])
+        .map((wIndex) => diagram.wordOrder[wIndex])
+        .map((id) => DiagramState.getTypedItem(diagram, "word", id).value.lexeme);
 }
 
-function generateLabelBorder(headLabel: string, index: number, positions: number[]): typeof ElementLabel {
-    let output = ElementLabel;
-    const position = positions[index];
-
-    if (index < positions.length - 1) {
-        const rightPosition = positions[index + 1];
-        if (rightPosition - position !== 1) {
-            output = withSideBorder(output, "right");
+function _placeElementData(data: ElementData, output: (ElementData | undefined)[]): void {
+    if (Array.isArray(data.index)) {
+        const [startIndex, endIndex] = data.index;
+        output[startIndex] = data;
+        for (let index = startIndex + 1; index < endIndex + 1; index++) {
+            output[index] = undefined;
         }
-    } else if (index === positions.length - 1) {
-        output = withSideBorder(output, "right");
-    }
-
-    if (index === 0) {
-        output = withHeadLabel(output, headLabel);
     } else {
-        const leftPosition = positions[index - 1];
-        if (position - leftPosition !== 1) {
-            output = withSideBorder(output, "left");
-        }
+        output[data.index] = data;
     }
-    return output;
 }
 
-function getElementSelectNodes(item: DiagramStateItem): ElementSelectNode[] {
-    if (item.type === "word") {
-        throw "cannot display children of word element";
-    }
-    const output: ElementSelectNode[] = [];
-    const itemValue = item.value as unknown as Record<string, undefined | ElementReference | ElementReference[]>;
-    const entries = Object.entries(getElementDefinition(item.type));
-    for (let i = 0; i < entries.length; i++) {
-        const [propertyName] = entries[i];
-        const propertyValue = itemValue[propertyName];
-        if (propertyValue === undefined) {
-            continue;
-        } else if (Array.isArray(propertyValue)) {
-            propertyValue.forEach(({ id }) => {
-                output.push({
-                    id: id,
-                    property: propertyName,
-                    type: item.type
-                });
-            });
-        } else {
-            output.push({
-                id: propertyValue.id,
-                property: propertyName,
-                type: item.type
-            });
-        }
-    }
-    return output;
-}
-
-function addItemLabels(model: DisplayModel, node: ElementSelectNode, labelClick: LabelClickFunction): (SimpleComponent | undefined)[] {
-    const output: (SimpleComponent | undefined)[] = model.word.map(() => undefined);
-    const positions = model.elements[node.id].words;
-    positions.forEach((position, index) => {
-        const { header, color } = ElementDisplayInfo.getDisplayInfo(node.type);
-        let Label = generateLabelBorder(header, index, positions);
-        Label = withOnClick(Label, () => labelClick(node));
-        const fnCmp: SimpleComponent = ({ children }) => <Label color={color}>{children}</Label>;
-        fnCmp.displayName = Label.displayName;
-        output[position] = fnCmp;
-    });
-    return output;
-}
-
-function combineArrays<T>(array1: (T | undefined)[], array2: (T | undefined)[]): (T | undefined)[] {
-    if (array1.length !== array2.length) {
-        throw `Arrays are of unequal lengths\narray1: ${array1.length}\narray2: ${array2.length}`;
-    }
-    const output: (T | undefined)[] = [];
-    for (let i = 0; i < array1.length; i++) {
-        let state = NEITHER_PRESENT;
-        const left = array1[i];
-        const right = array2[i];
-        if (left !== undefined) {
-            state |= LEFT_PRESENT;
-        }
-        if (right !== undefined) {
-            state |= RIGHT_PRESENT;
-        }
-        let item: T | undefined;
-        switch (state) {
-            case NEITHER_PRESENT:
-                item = undefined;
-                break;
-            case LEFT_PRESENT:
-                item = left;
-                break;
-            case RIGHT_PRESENT:
-                item = right;
-                break;
-            case BOTH_PRESENT:
-                throw `array1 and array2 both contain items at index ${i}`;
-            default:
-                throw `unexpected state at index ${i}`;
-        }
-        output.push(item);
-    }
-    return output;
-}
-
-function createSelectedLabels(diagram: DiagramState, { displayModel, selectedItem }: DisplayModelContext, labelClick: LabelClickFunction): (SimpleComponent | undefined)[] {
-    let output: (SimpleComponent | undefined)[] = diagram.wordOrder.map(() => undefined);
-    if (selectedItem === undefined) {
-        return output;
-    }
-    const finalSelectedItem = selectedItem[selectedItem.length - 1];
-    const diagramItem = DiagramState.getItem(diagram, finalSelectedItem.id);
-    const nodes = getElementSelectNodes(diagramItem);
-    nodes.forEach((node) => {
-        const labels = addItemLabels(displayModel, node, () => labelClick(node));
-        output = combineArrays(output, labels);
-    });
-    return output;
-}
-
-const NakedWord = makeRefComponent<HTMLDivElement, SimpleComponentProps>("NakedWord", ({ children }, ref) => {
-    return (
-        <div
-            className={accessClassName(styles, "nakedWord")}
-            ref={ref}
-        >
-            <span>{children}</span>
-        </div>
-    );
-});
-
-function createTopLabels(diagram: DiagramState, { displayModel, topFilter }: DisplayModelContext, labelClick: LabelClickFunction): (SimpleComponent | undefined)[] {
-    let output: (SimpleComponent | undefined)[] = diagram.wordOrder.map(() => undefined);
-    if (topFilter === undefined) {
-        return output;
-    }
-    const ids = displayModel[topFilter];
-    ids.forEach((id) => {
-        const item = DiagramState.getItem(diagram, id);
-        const node: ElementSelectNode = {
+function _getElementData(diagram: DiagramState, model: DisplayModel, id: ElementId): ElementData[] {
+    const output: ElementData[] = [];
+    const elementType = DiagramState.getItem(diagram, id).type;
+    const element = model[id];
+    const headWIndex = element.words[0];
+    const headData: ElementData = {
+        head: true,
+        selected: false,
+        id: id,
+        key: `${id}-0`,
+        lexemes: getLexemes(diagram, headWIndex),
+        type: elementType,
+        index: headWIndex
+    };
+    output.push(headData);
+    for (let i = 1; i < element.words.length; i++) {
+        const tailWIndex = element.words[i];
+        const tailData: ElementData = {
+            head: false,
+            selected: false,
             id: id,
-            type: item.type
+            key: `${id}-${i}`,
+            lexemes: getLexemes(diagram, tailWIndex),
+            type: elementType,
+            index: tailWIndex
         };
-        const labels = addItemLabels(displayModel, node, labelClick);
-        output = combineArrays(output, labels);
-    });
+        output.push(tailData);
+    }
     return output;
 }
 
-function createDisplayLabels(diagram: DiagramState, model: DisplayModelContext, labelClick: LabelClickFunction, nakedWordClick: NakedWordClickFunction): SimpleComponent[] {
-    const output = model.selectedItem === undefined
-        ? createTopLabels(diagram, model, labelClick)
-        : createSelectedLabels(diagram, model, labelClick);
-    for (let position = 0; position < diagram.wordOrder.length; position++) {
-        if (output[position] !== undefined) {
-            continue;
-        }
-        output[position] = withOnClick(NakedWord, nakedWordClick);
+function _populate(diagram: DiagramState, model: DisplayModel, id: ElementId, selectState: ElementSelectState | undefined, output: (ElementData | undefined)[]): void {
+    switch (selectState) {
+        case "select":
+            _populateSelected(diagram, model, id, output);
+            break;
+        case "expand":
+            _populateExpanded(diagram, model, id, output);
+            break;
+        case undefined:
+            _getElementData(diagram, model, id).forEach((data) => _placeElementData(data, output));
+            break;
+        default:
+            throw " ";
     }
-    return output as SimpleComponent[];
 }
 
-export const EditDiagram: React.VFC = () => {
+function _populateExpanded(diagram: DiagramState, displayModel: DisplayModel, id: ElementId, output: (ElementData | undefined)[]): void {
+    const displayElement = displayModel[id];
+    if (displayElement.properties === undefined) {
+        throw "";
+    }
+    Object.values(displayElement.properties)
+        .flat()
+        .forEach((childId) => _populate(diagram, displayModel, childId, undefined, output));
+}
+
+function _populateSelected(diagram: DiagramState, displayModel: DisplayModel, id: ElementId, output: (ElementData | undefined)[]): void {
+    _getElementData(diagram, displayModel, id)
+        .map((data) => { return { ...data, selected: true }; })
+        .forEach((data) => _placeElementData(data, output));
+}
+
+function _selectedItem(diagram: DiagramState, displayModel: DisplayModel, selectedItem: SelectedElement | undefined, output: (ElementData | undefined)[]): void {
+    if (selectedItem === undefined) {
+        return;
+    }
+    const lastItem = selectedItem[selectedItem.length - 1];
+    _populate(diagram, displayModel, lastItem.id, lastItem.state, output);
+    for (let index = selectedItem.length - 2; index >= 0; index--) {
+        const itemId = selectedItem[index].id;
+        const childId = selectedItem[index + 1].id;
+        const item = displayModel[itemId];
+        if (item.properties === undefined) {
+            throw "";
+        }
+        Object.values(item.properties)
+            .flat()
+            .filter((id) => id !== childId)
+            .forEach((id) => _populate(diagram, displayModel, id, undefined, output));
+    }
+}
+
+function _elementFilter(diagram: DiagramState, displayModel: DisplayModel, elementFilter: ElementCategory, output: (ElementData | undefined)[]): void {
+    Object.entries(displayModel)
+        .filter(([, { category, ref }]) => {
+            return category === elementFilter
+                && ref !== undefined
+                && displayModel[ref].category !== category;
+        })
+        .forEach(([id]) => _populate(diagram, displayModel, id, undefined, output));
+}
+
+function _fillOutput(diagram: DiagramState, displayModel: DisplayModel, output: (ElementData | undefined)[]): void {
+    let index = 0;
+    while (index < output.length) {
+        const data = output[index];
+        if (data === undefined) {
+            const id = diagram.wordOrder[index];
+            _populate(diagram, displayModel, id, undefined, output);
+            index++;
+        } else {
+            let jumpCnt = 1;
+            if (Array.isArray(data.index)) {
+                const [startIndex, endIndex] = data.index;
+                jumpCnt = endIndex - startIndex + 1;
+            }
+            index += jumpCnt;
+        }
+    }
+}
+
+function getElementData(diagram: DiagramState, displayModel: DisplayModel, elementFilter: ElementCategory | undefined, selectedItem: SelectedElement | undefined): ElementData[] {
+    const output: (ElementData | undefined)[] = diagram.wordOrder.map(() => undefined);
+    _elementFilter(
+        diagram,
+        displayModel,
+        elementFilter === undefined ? "word" : elementFilter,
+        output
+    );
+    _selectedItem(diagram, displayModel, selectedItem, output);
+    _fillOutput(diagram, displayModel, output);
+
+    return output.filter((x) => x !== undefined) as ElementData[];
+}
+
+export type ElementData = {
+    head: boolean;
+    id: ElementId;
+    key: string;
+    type: ElementType;
+    lexemes: string[];
+    selected: boolean;
+    index: WordIndices[number];
+}
+
+export interface EditDiagramProps {
+    elementBuildFn?: (Component: RefComponent<HTMLSpanElement>, data: ElementData) => RefComponent<HTMLSpanElement>;
+}
+
+function withSpace(Component: RefComponent<HTMLSpanElement>): RefComponent<HTMLSpanElement> {
+    return makeRefComponent("withSpace", (_, ref) => (
+        <>
+            <Component ref={ref}/>
+            <Space/>
+        </>
+    ));
+}
+
+export const EditDiagram = makeRefComponent<HTMLDivElement, EditDiagramProps>("EditDiagram", ({ elementBuildFn }, ref) => {
     const diagram = useContext(DiagramStateContext);
     const model = useContext(DisplayModelContext);
-    const state = diagram.state.currState;
-    const reset = () => model.setSelectedItem(undefined);
-    const ref = useOutsideClick<HTMLDivElement>(reset);
+    const elementData = getElementData(
+        diagram.state.currState,
+        diagram.model,
+        model.elementFilter,
+        model.selectedItem
+    );
 
-    function labelClickFunction(node: ElementSelectNode) {
-        let newSelectedItem: SelectedElement;
-        if (model.selectedItem === undefined) {
-            if (!isHeadElementSelectNode(node)) {
-                throw "";
-            }
-            newSelectedItem = [node];
-        } else {
-            if (!isTailElementSelectNode(node)) {
-                throw "";
-            }
-            newSelectedItem = [...model.selectedItem, node];
-        }
-        model.setSelectedItem(newSelectedItem);
+    function createElement(data: ElementData): RefComponent<HTMLSpanElement> {
+        const displayInfo = ElementDisplayInfo.getDisplayInfo(data.type);
+        const output = makeRefComponent<HTMLSpanElement>("", (_0, ref0) => (
+            <WordLabel color={displayInfo.color} ref={ref0}>
+                {data.lexemes.map((lexeme, index) => {
+                    const key = `${data.key}-${index}`;
+                    let Component = makeRefComponent<HTMLSpanElement>("", (_1, ref1) => (
+                        <Word ref={ref1}>
+                            {(data.head && index === 0) &&
+                                <HeadLabel>{displayInfo.header}</HeadLabel>
+                            }
+                            {lexeme}
+                        </Word>
+                    ));
+                    Component.displayName = key;
+                    if (index < data.lexemes.length - 1) {
+                        Component = withSpace(Component);
+                    }
+                    return <Component key={key}/>;
+                })}
+            </WordLabel>
+        ));
+        return elementBuildFn ? elementBuildFn(output, data) : output;
     }
 
-    const items = createDisplayLabels(state, model, labelClickFunction, reset);
     return (
-        <div
-            className={accessClassName(styles, "editDiagram")}
-            ref={ref}
-        >
-            {state.wordOrder.map((id, index) => {
-                const Component = items[index];
-                const lexeme = getLexeme(state, index);
-                return <Component key={index}>{lexeme}</Component>;
+        <div ref={ref}>
+            {elementData.map((data, index) => {
+                let Component = createElement(data);
+                if (index < elementData.length - 1) {
+                    Component = withSpace(Component);
+                }
+                return <Component key={data.key}/>;
             })}
         </div>
     );
-};
+});
