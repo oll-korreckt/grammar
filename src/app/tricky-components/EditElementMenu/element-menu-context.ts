679fc9446ca6e16d4342a9e9ec8952e62c8c0eea
ok <oll.korreckt@gmail.com>
2021-08-13 13:44:35 -0400

    Deleted EditElementMenu component since it shouldn't be needed anymore.

--- src/app/tricky-components/EditElementMenu/element-menu-context.ts
+++
@@ -1,8 +0,0 @@
-import { ElementId } from "@domain/language";
-import { createContext } from "react";
-
-export interface ElementMenuContext {
-    id: ElementId;
-}
-
-export const ElementMenuContext = createContext<ElementMenuContext>({ id: "" });
