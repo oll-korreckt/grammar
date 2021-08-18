d05e1e01fd0604aef279df395884022afcb0bb4b
ok <oll.korreckt@gmail.com>
2021-08-18 16:30:04 -0400

    Created ElementProperty component

---
+++ src/app/basic-components/ElementProperty/ElementProperty.tsx
@@ -0,0 +1,36 @@
+import { accessClassName } from "@app/utils";
+import { makeRefComponent } from "@app/utils/hoc";
+import React from "react";
+import { IoPricetag, IoPricetagOutline, IoPricetags, IoPricetagsOutline } from "react-icons/io5";
+import styles from "./_styles.scss";
+
+export interface ElementPropertyProps {
+    children: string;
+    selectionType: "single" | "multi";
+    status: "complete" | "incomplete" | "error";
+    state: "selected" | "unselected";
+}
+
+export const ElementProperty = makeRefComponent<HTMLDivElement, ElementPropertyProps>("ElementProperty", ({ children, selectionType, state, status }, ref) => {
+    const classNames = ["elementProperty", state];
+    if (status === "error") {
+        classNames.push("error");
+    }
+    return (
+        <div
+            className={accessClassName(styles, ...classNames)}
+            ref={ref}
+        >
+            {children}
+            <span>&nbsp;</span>
+            {selectionType === "single"
+                ? status === "complete"
+                    ? <IoPricetag/>
+                    : <IoPricetagOutline/>
+                : status === "complete"
+                    ? <IoPricetags/>
+                    : <IoPricetagsOutline/>
+            }
+        </div>
+    );
+});
