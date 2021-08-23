4a6b257d2132eddf098db1488c671457b4a5ec25
ok <oll.korreckt@gmail.com>
2021-08-23 11:50:58 -0400

    Added functionality to prevent Word components from being highlighted on double clicks

--- src/app/basic-components/Word/Word.tsx
+++ src/app/basic-components/Word/Word.tsx
@@ -3,34 +3,63 @@ import { makeRefComponent } from "@app/utils/hoc";
 import React, { ReactNode } from "react";
 import styles from "./_styles.scss";
 
-export const Space: React.VFC = () => {
-    return <span className={accessClassName(styles, "word")}>&#32;</span>;
-};
+function cancelHighlightDoubleClick(ev: React.MouseEvent): void {
+    if (ev.detail > 1) {
+        ev.preventDefault();
+    }
+}
+
+export const Space: React.VFC = () => (
+    <span
+        className={accessClassName(styles, "word")}
+        onMouseDown={cancelHighlightDoubleClick}
+    >
+        &#32;
+    </span>
+);
 
 export interface WordProps {
     children: ReactNode;
 }
 
-export const Word = makeRefComponent<HTMLSpanElement, WordProps>("Word", ({ children }, ref) => {
-    return <span className={accessClassName(styles, "word")} ref={ref}>{children}</span>;
-});
+export const Word = makeRefComponent<HTMLSpanElement, WordProps>("Word", ({ children }, ref) => (
+    <span
+        className={accessClassName(styles, "word")}
+        ref={ref}
+        onMouseDown={cancelHighlightDoubleClick}
+    >
+        {children}
+    </span>
+));
 
 export interface HeadLabelProps {
     children: string;
 }
 
-export const HeadLabel = makeRefComponent<HTMLDivElement, HeadLabelProps>("HeadLabel", ({ children }, ref) => {
-    return <div className={accessClassName(styles, "headLabel")} ref={ref}>{children}</div>;
-});
+export const HeadLabel = makeRefComponent<HTMLDivElement, HeadLabelProps>("HeadLabel", ({ children }, ref) => (
+    <div
+        className={accessClassName(styles, "headLabel")}
+        ref={ref}
+        onMouseDown={cancelHighlightDoubleClick}
+    >
+        {children}
+    </div>
+));
 
 export interface WordLabelProps {
     children: ReactNode;
     color: Colors;
 }
 
-export const WordLabel = makeRefComponent<HTMLSpanElement, WordLabelProps>("WordLabel", ({ color, children }, ref) => {
-    return <span className={accessClassName(styles, "wordLabel", color)} ref={ref}>{children}</span>;
-});
+export const WordLabel = makeRefComponent<HTMLSpanElement, WordLabelProps>("WordLabel", ({ color, children }, ref) => (
+    <span
+        className={accessClassName(styles, "wordLabel", color)}
+        ref={ref}
+        onMouseDown={cancelHighlightDoubleClick}
+    >
+        {children}
+    </span>
+));
 
 export function withSpace(Component: typeof Word): typeof Word {
     return makeRefComponent("withSpace", (props, ref) => (
