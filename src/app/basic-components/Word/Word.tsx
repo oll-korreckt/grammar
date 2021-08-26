e6755109803fa74ea8841bdd68d90c56ab3e0551
ok <oll.korreckt@gmail.com>
2021-08-26 15:02:20 -0400

    Added fade classes and some extra hoc functions for Word

--- src/app/basic-components/Word/Word.tsx
+++ src/app/basic-components/Word/Word.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/ban-types */
 import { accessClassName, Colors } from "@app/utils";
-import { makeRefComponent } from "@app/utils/hoc";
+import { makeRefComponent, makeRefHoc, RefComponent } from "@app/utils/hoc";
 import React, { ReactNode } from "react";
 import styles from "./_styles.scss";
 
@@ -41,6 +42,7 @@ export const HeadLabel = makeRefComponent<HTMLDivElement, HeadLabelProps>("HeadL
         className={accessClassName(styles, "headLabel")}
         ref={ref}
         onMouseDown={cancelHighlightDoubleClick}
+        id="headLabel"
     >
         {children}
     </div>
@@ -68,4 +70,68 @@ export function withSpace(Component: typeof Word): typeof Word {
             <Space/>
         </>
     ));
+}
+
+const colors: Colors[] = [
+    "color1",
+    "color2",
+    "color3",
+    "color4",
+    "color5",
+    "color6",
+    "color7",
+    "color8",
+    "color9",
+    "color10",
+    "color11",
+    "color12"
+];
+
+function getColorClass(color: Colors): string {
+    return accessClassName(styles, color);
+}
+
+function getFadeName(color: Colors): string {
+    return `${color}Fade`;
+}
+
+export function withDisable<TElement extends HTMLElement, TProps = {}>(Component: RefComponent<TElement, TProps>): RefComponent<TElement, TProps> {
+    return makeRefHoc(Component, "withDisable", (_, element) => {
+        element.classList.remove(
+            getColorClass("color1"),
+            getColorClass("color2"),
+            getColorClass("color3"),
+            getColorClass("color4"),
+            getColorClass("color5"),
+            getColorClass("color6"),
+            getColorClass("color7"),
+            getColorClass("color8"),
+            getColorClass("color9"),
+            getColorClass("color10"),
+            getColorClass("color11"),
+            getColorClass("color12")
+        );
+        const header = element.querySelector("#headLabel");
+        if (header !== null) {
+            element.children[0].removeChild(header);
+        }
+    });
+}
+
+export function withFade<TElement extends HTMLElement, TProps = {}>(Component: RefComponent<TElement, TProps>): RefComponent<TElement, TProps> {
+    const fadeMap: Record<string, string> = {};
+    colors.forEach((color) => {
+        fadeMap[getColorClass(color)] = getFadeName(color);
+    });
+    return makeRefHoc(Component, "withFade", (_, element) => {
+        for (let index = 0; index < element.classList.length; index++) {
+            const className = element.classList[index];
+            const newName: string | undefined = fadeMap[className];
+            if (newName !== undefined) {
+                const newClassName = accessClassName(styles, newName);
+                element.classList.replace(className, newClassName);
+                break;
+            }
+        }
+    });
 }
