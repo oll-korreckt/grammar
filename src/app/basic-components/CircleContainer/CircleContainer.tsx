76b8d659d12176e21195d2854b199240bacf24e7
ok <oll.korreckt@gmail.com>
2021-09-15 15:43:32 -0400

    Created CircleContainer component for getting text to wrap in a circular manner.

---
+++ src/app/basic-components/CircleContainer/CircleContainer.tsx
@@ -0,0 +1,16 @@
+import { accessClassName } from "@app/utils";
+import { makeRefComponent } from "@app/utils/hoc";
+import React, { ReactNode } from "react";
+import styles from "./_styles.scss";
+
+export interface CircleContainerProps {
+    children?: ReactNode;
+}
+
+export const CircleContainer = makeRefComponent<HTMLDivElement, CircleContainerProps>("CircleContainer", ({ children }, ref) => (
+    <div className={accessClassName(styles, "container")} ref={ref}>
+        <div className={accessClassName(styles, "left")}></div>
+        <div className={accessClassName(styles, "right")}></div>
+        {children}
+    </div>
+));
