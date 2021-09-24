22958a47d5992476cf7476a9fc01a6f81c3a36c2
ok <oll.korreckt@gmail.com>
2021-09-24 15:38:58 -0400

    Made WordViewContext compatible with new navigation mode

--- src/app/utils/__tests__/word-view-context.tests.ts
+++ src/app/utils/__tests__/word-view-context.tests.ts
@@ -1,47 +1,80 @@
 import { createState, Ids } from "@app/testing";
+import { ElementCategory, ElementId } from "@domain/language";
 import { assert } from "chai";
-import { DiagramState, SelectedNodeChain, SelectNode } from "..";
+import { DiagramState, DisplayModel, SelectedElementChain, _getVisibleElements, SelectedNodeChain } from "..";
 
 describe("word-view-context", () => {
     let state: DiagramState;
+    let model: DisplayModel;
     beforeAll(() => {
         state = createState();
+        model = DisplayModel.init(state);
     });
 
-    describe("SelectNode.getParent", () => {
-        test("has result", () => {
-            const result = SelectNode.getParent(state, Ids.jumpsVerbPhrase);
-            const expected: SelectNode = {
-                id: Ids.indClause,
-                type: "independentClause",
-                state: "expand"
-            };
+    describe("_getVisibleElements", () => {
+        function getResult(elementCategory: ElementCategory, selectedElement?: ElementId): ElementId[] {
+            return _getVisibleElements(state, model, elementCategory, selectedElement)
+                .map(({ id }) => id);
+        }
+
+        test("clause - no selection", () => {
+            const result = getResult("clause");
+            assert.deepStrictEqual(result, [Ids.indClause]);
+        });
+
+        test("clause - jumpsVerbPhrase", () => {
+            const result = getResult("clause", Ids.jumpsVerbPhrase);
+            assert.deepStrictEqual(
+                result,
+                [Ids.foxNounPhrase, Ids.jumpsVerb, Ids.overPrepPhrase]
+            );
+        });
+
+        test("phrase - quickBrownAdjPhrase", () => {
+            const result = getResult("phrase", Ids.quickBrownAdjPhrase);
+            const expected = [
+                Ids.the1Det,
+                Ids.quickBrownAdj,
+                Ids.foxNoun,
+                Ids.jumpsVerbPhrase
+            ];
             assert.deepStrictEqual(result, expected);
         });
 
-        test("undefined result", () => {
-            const result = SelectNode.getParent(state, Ids.indClause);
-            assert.isUndefined(result);
+        test("partOfSpeech - quickBrownAdj", () => {
+            const result = getResult("partOfSpeech", Ids.quickBrownAdj);
+            const expected = [
+                Ids.the1Det,
+                Ids.quick,
+                Ids.brown,
+                Ids.foxNoun,
+                Ids.jumpsVerb,
+                Ids.overPrep,
+                Ids.the2Det,
+                Ids.lazyAdj,
+                Ids.dogNoun
+            ];
+            assert.deepStrictEqual(result, expected);
         });
     });
 
     describe("SelectedNodeChain.generateChain", () => {
         test("5-layer deep", () => {
-            const result = SelectedNodeChain.generateChain(state, Ids.brown, "select");
-            const expected: SelectedNodeChain = [
-                { id: Ids.indClause, type: "independentClause", state: "expand" },
-                { id: Ids.foxNounPhrase, type: "nounPhrase", state: "expand", property: "subject" },
-                { id: Ids.quickBrownAdjPhrase, type: "adjectivePhrase", state: "expand", property: "modifiers" },
-                { id: Ids.quickBrownAdj, type: "adjective", state: "expand", property: "head" },
-                { id: Ids.brown, type: "word", state: "select", property: "words" }
+            const result = SelectedNodeChain.generateChain(state, "clause", Ids.brown);
+            const expected: SelectedElementChain = [
+                { id: Ids.indClause, type: "independentClause" },
+                { id: Ids.foxNounPhrase, type: "nounPhrase" },
+                { id: Ids.quickBrownAdjPhrase, type: "adjectivePhrase" },
+                { id: Ids.quickBrownAdj, type: "adjective" },
+                { id: Ids.brown, type: "word" }
             ];
             assert.deepStrictEqual(result, expected);
         });
 
         test("top layer", () => {
-            const result = SelectedNodeChain.generateChain(state, Ids.indClause, "expand");
-            const expected: SelectedNodeChain = [
-                { id: Ids.indClause, type: "independentClause", state: "expand" }
+            const result = SelectedNodeChain.generateChain(state, "clause", Ids.indClause);
+            const expected: SelectedElementChain = [
+                { id: Ids.indClause, type: "independentClause" }
             ];
             assert.deepStrictEqual(result, expected);
         });
--- src/app/utils/word-view-context.ts
+++ src/app/utils/word-view-context.ts
@@ -1,6 +1,6 @@
 import { ElementCategory, ElementId, ElementType } from "@domain/language";
 import { createContext } from "react";
-import { DiagramState, DiagramStateContext, DiagramStateItem } from ".";
+import { DiagramState, DiagramStateContext } from ".";
 import { DisplayModel, WordIndices, WordRange } from "./display-model";
 
 export type ElementData = {
@@ -13,56 +13,22 @@ export type ElementData = {
     index: WordIndices[number];
 }
 
-export type ElementSelectState = "expand" | "select";
-
-export type SelectNode = {
+export interface ChainItem {
     id: ElementId;
     type: ElementType;
-    state: ElementSelectState;
 }
 
-function getParent(state: DiagramState, id: ElementId): SelectNode | undefined {
-    const item = DiagramState.getItem(state, id);
-    if (item.ref === undefined) {
-        return undefined;
-    }
-    const parent = DiagramState.getItem(state, item.ref);
-    return {
-        id: item.ref,
-        type: parent.type,
-        state: "expand"
-    };
-}
-
-export const SelectNode = {
-    getParent: getParent
-};
-
-export interface HeadSelectNode {
-    id: ElementId;
-    state: ElementSelectState;
-    type: ElementType;
-}
-
-export interface TailSelectNode extends HeadSelectNode {
-    state: ElementSelectState;
-    property: string | [string, string];
-}
-
-export type SelectedNodeChain = [HeadSelectNode, ...TailSelectNode[]];
-export type WordViewStage = "category" | "syntax";
+export type SelectedElementChain = ChainItem[];
 export type WordViewMode = "navigate" | "label" | "edit" | "delete";
 
 export interface WordViewContext {
     elementCategory: ElementCategory;
-    mode: WordViewMode;
     visibleElements: ElementData[];
-    selectedNode?: SelectNode;
+    selectedElement?: ElementId;
 }
 
 export const WordViewContext = createContext<WordViewContext>({
-    elementCategory: "word",
-    mode: "navigate",
+    elementCategory: "clause",
     visibleElements: []
 });
 
@@ -121,20 +87,8 @@ function _getElementData(diagram: DiagramState, model: DisplayModel, id: Element
 }
 
 
-function _populate(diagram: DiagramState, model: DisplayModel, id: ElementId, selectState: ElementSelectState | undefined, output: (ElementData | undefined)[]): void {
-    switch (selectState) {
-        case "select":
-            _populateSelected(diagram, model, id, output);
-            break;
-        case "expand":
-            _populateExpanded(diagram, model, id, output);
-            break;
-        case undefined:
-            _getElementData(diagram, model, id).forEach((data) => _placeElementData(data, output));
-            break;
-        default:
-            throw " ";
-    }
+function _populate(diagram: DiagramState, model: DisplayModel, id: ElementId, output: (ElementData | undefined)[]): void {
+    _getElementData(diagram, model, id).forEach((data) => _placeElementData(data, output));
 }
 
 function _populateExpanded(diagram: DiagramState, displayModel: DisplayModel, id: ElementId, output: (ElementData | undefined)[]): void {
@@ -144,26 +98,20 @@ function _populateExpanded(diagram: DiagramState, displayModel: DisplayModel, id
         : {};
     Object.values(properties)
         .flat()
-        .forEach((childId) => _populate(diagram, displayModel, childId, undefined, output));
+        .forEach((childId) => _populate(diagram, displayModel, childId, output));
 }
 
-function _populateSelected(diagram: DiagramState, displayModel: DisplayModel, id: ElementId, output: (ElementData | undefined)[]): void {
-    _getElementData(diagram, displayModel, id)
-        .map((data) => { return { ...data, selected: true }; })
-        .forEach((data) => _placeElementData(data, output));
-}
-
-function _selectedItem(diagram: DiagramState, displayModel: DisplayModel, selectedNode: SelectNode | undefined, output: (ElementData | undefined)[]): void {
-    if (selectedNode === undefined) {
+function _selectedItem(diagram: DiagramState, displayModel: DisplayModel, elementCategory: ElementCategory, selectedElement: ElementId | undefined, output: (ElementData | undefined)[]): void {
+    if (selectedElement === undefined) {
         return;
     }
     const selectedItem = SelectedNodeChain.generateChain(
         diagram,
-        selectedNode.id,
-        selectedNode.state
+        elementCategory,
+        selectedElement
     );
     const lastItem = selectedItem[selectedItem.length - 1];
-    _populate(diagram, displayModel, lastItem.id, lastItem.state, output);
+    _populateExpanded(diagram, displayModel, lastItem.id, output);
     for (let index = selectedItem.length - 2; index >= 0; index--) {
         const itemId = selectedItem[index].id;
         const childId = selectedItem[index + 1].id;
@@ -174,23 +122,26 @@ function _selectedItem(diagram: DiagramState, displayModel: DisplayModel, select
         Object.values(item.properties)
             .flat()
             .filter((id) => id !== childId)
-            .forEach((id) => _populate(diagram, displayModel, id, undefined, output));
+            .forEach((id) => _populate(diagram, displayModel, id, output));
     }
 }
 
 function _elementFilter(diagram: DiagramState, displayModel: DisplayModel, elementFilter: ElementFilterFunction, output: (ElementData | undefined)[]): void {
     Object.entries(displayModel)
         .filter(([, { category, ref }]) => {
-            const correctType = elementFilter(category);
-            if (!correctType) {
+            if (!elementFilter(category)) {
                 return false;
             }
             if (ref === undefined) {
                 return true;
             }
-            return !correctType;
+            const parentCategory = displayModel[ref].category;
+            if (elementFilter(parentCategory)) {
+                return false;
+            }
+            return true;
         })
-        .forEach(([id]) => _populate(diagram, displayModel, id, undefined, output));
+        .forEach(([id]) => _populate(diagram, displayModel, id, output));
 }
 
 function _fillOutput(diagram: DiagramState, displayModel: DisplayModel, output: (ElementData | undefined)[]): void {
@@ -199,7 +150,7 @@ function _fillOutput(diagram: DiagramState, displayModel: DisplayModel, output: 
         const data = output[index];
         if (data === undefined) {
             const id = diagram.wordOrder[index];
-            _populate(diagram, displayModel, id, undefined, output);
+            _populate(diagram, displayModel, id, output);
             index++;
         } else {
             let jumpCnt = 1;
@@ -212,93 +163,52 @@ function _fillOutput(diagram: DiagramState, displayModel: DisplayModel, output: 
     }
 }
 
-function createFilterFn(category: ElementCategory): ElementFilterFunction {
-    let filterArray: ElementCategory[];
-    switch (category) {
-        case "word":
-            filterArray = ["word"];
-            break;
-        case "partOfSpeech":
-            filterArray = ["word", "partOfSpeech"];
-            break;
-        case "phrase":
-            filterArray = ["word", "partOfSpeech", "phrase"];
-            break;
-        case "clause":
-            filterArray = ["word", "partOfSpeech", "phrase", "clause"];
-            break;
-        default:
-            throw `unhandled stage '${category}'`;
-    }
-    const filterObj = new Set(filterArray);
-    return (cat) => filterObj.has(cat);
-}
-
-export function _getVisibleElements(diagram: DiagramState, displayModel: DisplayModel, elementCategory: ElementCategory, selectedNode: SelectNode | undefined): ElementData[] {
+export function _getVisibleElements(diagram: DiagramState, displayModel: DisplayModel, elementCategory: ElementCategory, selectedElement: ElementId | undefined): ElementData[] {
     const output: (ElementData | undefined)[] = diagram.wordOrder.map(() => undefined);
     _elementFilter(
         diagram,
         displayModel,
-        createFilterFn(elementCategory),
+        ElementCategory.getLayerFilter(elementCategory),
         output
     );
-    _selectedItem(diagram, displayModel, selectedNode, output);
+    _selectedItem(diagram, displayModel, elementCategory, selectedElement, output);
     _fillOutput(diagram, displayModel, output);
 
     return output.filter((x) => x !== undefined) as ElementData[];
 }
 
-export function createWordViewContext(state: DiagramStateContext, data: Omit<WordViewContext, "visibleElements">): WordViewContext {
+export function createWordViewContext(state: DiagramStateContext, elementCategory: ElementCategory, selectedElement?: ElementId): WordViewContext {
     const visibleElements = _getVisibleElements(
         state.state,
         state.model,
-        data.elementCategory,
-        data.selectedNode
+        elementCategory,
+        selectedElement
     );
     return {
-        ...data,
+        elementCategory: elementCategory,
+        selectedElement: selectedElement,
         visibleElements: visibleElements
     };
 }
 
-function generateChain(state: DiagramState, id: ElementId, selectState: ElementSelectState): SelectedNodeChain {
-    type OutputNode = Omit<SelectNode, "state"> & { property?: string | [string, string]; };
-    let output: OutputNode[] = [];
+function generateChain(state: DiagramState, elementCategory: ElementCategory, id: ElementId): SelectedElementChain {
+    const filterFn = ElementCategory.getLayerFilter(elementCategory);
+    let output: ChainItem[] = [];
     let parentId: ElementId | undefined = id;
-    let childId: ElementId | undefined = undefined;
-    let childItem: DiagramStateItem | undefined = undefined;
     while (parentId !== undefined) {
         const parentItem = DiagramState.getItem(state, parentId);
-        const newNode: OutputNode = {
+        const parentCategory = ElementCategory.getElementCategory(parentItem.type);
+        if (!filterFn(parentCategory)) {
+            break;
+        }
+        const newNode: ChainItem = {
             id: parentId,
             type: parentItem.type
         };
-        if (childItem !== undefined) {
-            const properties = DiagramState.getReferencingProperties(
-                parentItem.type as Exclude<ElementType, "word">,
-                parentItem.value,
-                childId as ElementId
-            );
-            if (properties === undefined) {
-                throw `child '${childId}' is not referenced by '${parentId}'`;
-            }
-            const childNode = output[0];
-            childNode.property = properties;
-        }
         output = [newNode, ...output];
-        childItem = parentItem;
-        childId = parentId;
         parentId = parentItem.ref;
     }
-    return output.map((node, index) => {
-        const nodeSelectState: ElementSelectState = index === output.length - 1
-            ? selectState
-            : "expand";
-        return {
-            ...node,
-            state: nodeSelectState
-        };
-    }) as SelectedNodeChain;
+    return output;
 }
 
 export const SelectedNodeChain = {
