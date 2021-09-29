6c0ee320cd6eccb83e794a79bb406dbbde7690f7
ok <oll.korreckt@gmail.com>
2021-09-29 14:54:41 -0400

    Components no longer present

--- src/app/basic-components/index.ts
+++ src/app/basic-components/index.ts
@@ -1,6 +1,3 @@
-export * from "./XToggleButton/XToggleButton";
-export { XToggleRadioButton } from "./XToggleRadioButton/XToggleRadioButton";
-export type { XToggleRadioButtonProps, XToggleRadioButtonChild } from "./XToggleRadioButton/XToggleRadioButton";
 export * from "./Buttons/Buttons";
 export * from "./ButtonBar";
 export * from "./ElementFilter";
