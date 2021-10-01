79ebf28c5d099fdf92c26e938165597c418a7c13
ok <oll.korreckt@gmail.com>
2021-10-01 12:26:33 -0400

    Add location to ExtendedModeSelector for storing mode-specific menus

--- src/app/tricky-components/WordViewAssembly/_ExtendedModeSelector/ExtendedModeSelector.tsx
+++ src/app/tricky-components/WordViewAssembly/_ExtendedModeSelector/ExtendedModeSelector.tsx
@@ -1,22 +1,25 @@
 import { ModeSelector, ModeSelectorProps } from "@app/basic-components/ModeSelector";
 import { accessClassName, DiagramStateContext } from "@app/utils";
 import { makeRefComponent } from "@app/utils/hoc";
-import React, { useContext } from "react";
+import React, { useContext, PropsWithChildren } from "react";
 import styles from "./_styles.scss";
 
-export const ExtendedModeSelector = makeRefComponent<HTMLDivElement, ModeSelectorProps>("ExtendedModeSelector", (props, ref) => {
+export const ExtendedModeSelector = makeRefComponent<HTMLDivElement, PropsWithChildren<ModeSelectorProps>>("ExtendedModeSelector", ({ children, ...rest }, ref) => {
     const { progress } = useContext(DiagramStateContext);
     const percentage = progress.category.percentage * 0.4 + progress.syntax.percentage * 0.6;
     return (
         <div className={accessClassName(styles, "modeSelector")} ref={ref}>
-            <div className={accessClassName(styles, "progress")}>
-                <div
-                    style={{ width: `${percentage}%` }}
-                    className={accessClassName(styles, "percentage")}
-                >
+            {children}
+            <div className={accessClassName(styles, "modeSelectorBar")} ref={ref}>
+                <div className={accessClassName(styles, "progress")}>
+                    <div
+                        style={{ width: `${percentage}%` }}
+                        className={accessClassName(styles, "percentage")}
+                    >
+                    </div>
                 </div>
+                <ModeSelector {...rest}/>
             </div>
-            <ModeSelector {...props}/>
         </div>
     );
 });
