21886c4c22697a38ea89d34f48a44d50118380d7
ok <oll.korreckt@gmail.com>
2021-10-04 12:07:12 -0400

    Some minor changes pertaining to the way progress bar is displayed

--- src/app/tricky-components/WordViewAssembly/_ExtendedModeSelector/ExtendedModeSelector.tsx
+++ src/app/tricky-components/WordViewAssembly/_ExtendedModeSelector/ExtendedModeSelector.tsx
@@ -7,14 +7,20 @@ import styles from "./_styles.scss";
 export const ExtendedModeSelector = makeRefComponent<HTMLDivElement, PropsWithChildren<ModeSelectorProps>>("ExtendedModeSelector", ({ children, ...rest }, ref) => {
     const { progress } = useContext(DiagramStateContext);
     const percentage = progress.category.percentage * 0.4 + progress.syntax.percentage * 0.6;
+    const clippedPct = percentage === 0 ? 0.5 : percentage;
+    const pctClassNames = ["percentage"];
+    if (clippedPct < 100) {
+        pctClassNames.push("roundedEdges");
+    }
+
     return (
         <div className={accessClassName(styles, "modeSelector")} ref={ref}>
             {children}
             <div className={accessClassName(styles, "modeSelectorBar")} ref={ref}>
                 <div className={accessClassName(styles, "progress")}>
                     <div
-                        style={{ width: `${percentage}%` }}
-                        className={accessClassName(styles, "percentage")}
+                        style={{ width: `${clippedPct}%` }}
+                        className={accessClassName(styles, ...pctClassNames)}
                     >
                     </div>
                 </div>
