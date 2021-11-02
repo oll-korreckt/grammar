90247622c723eb6f4ced17f209a509975848a7fc
ok <oll.korreckt@gmail.com>
2021-11-02 09:36:21 -0400

    Created container for animating items and then removing them

---
+++ src/app/basic-components/AnimateThenDisappear/AnimateThenDisappear.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { AnimatePresence, HTMLMotionProps, motion } from "framer-motion";
+import styles from "./_styles.scss";
+import { accessClassName } from "@app/utils";
+
+export interface AnimateThenDisappearProps {
+    start: HTMLMotionProps<"div">["animate"];
+    end: HTMLMotionProps<"div">["exit"];
+    transition?: HTMLMotionProps<"div">["transition"];
+    children: React.ReactNode;
+}
+
+export const AnimateThenDisappear: React.VFC<AnimateThenDisappearProps> = ({ start, end, transition, children }) => {
+    const [run, setRun] = useState(false);
+
+    useEffect(() => {
+        setRun(true);
+    }, [start, end, children]);
+
+    useEffect(() => {
+        if (run) {
+            setRun(false);
+        }
+    }, [run]);
+
+    return (
+        <AnimatePresence>
+            {run &&
+                <motion.div
+                    className={accessClassName(styles, "container")}
+                    animate={start}
+                    exit={end}
+                    transition={transition}
+                >
+                    {children}
+                </motion.div>
+            }
+        </AnimatePresence>
+    );
+};
