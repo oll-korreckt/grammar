37bf725f4c879b2a423fdab6461cfef5064075cc
ok <oll.korreckt@gmail.com>
2021-12-14 11:17:57 -0500

    Modfied to create visual distinction between enabled and selected

--- src/app/basic-components/NavBar/NavBar.tsx
+++ src/app/basic-components/NavBar/NavBar.tsx
@@ -1,11 +1,13 @@
 import { accessClassName, Stage } from "@app/utils";
 import { makeRefComponent } from "@app/utils/hoc";
+import { AnimateSharedLayout, motion } from "framer-motion";
 import React from "react";
 import styles from "./_styles.scss";
 
 export interface NavBarProps {
     stage: Stage;
-    onStageChange?: (stage: Stage) => void;
+    enabledStages?: ["input"] | ["input", "label"];
+    onStageClick?: (stage: Stage) => void;
 }
 
 interface Item {
@@ -13,7 +15,10 @@ interface Item {
     stage: Stage;
 }
 
-export const NavBar = makeRefComponent<HTMLDivElement, NavBarProps>("NavBar", ({ stage, onStageChange }, ref) => {
+export const NavBar = makeRefComponent<HTMLDivElement, NavBarProps>("NavBar", ({ stage, enabledStages, onStageClick }, ref) => {
+    const enabledStagesSet: Set<Stage> = enabledStages !== undefined
+        ? new Set(enabledStages)
+        : new Set(["input"]);
     const items: Item[] = [
         { label: "Input", stage: "input" },
         { label: "Label", stage: "label" }
@@ -24,21 +29,55 @@ export const NavBar = makeRefComponent<HTMLDivElement, NavBarProps>("NavBar", ({
             ref={ref}
             className={accessClassName(styles, "container")}
         >
-            {items.map((item) => {
-                return (
-                    <div
-                        key={item.stage}
-                        className={accessClassName(
-                            styles,
-                            "item",
-                            item.stage === stage ? "selected" : "unselected"
-                        )}
-                        onClick={() => onStageChange && onStageChange(item.stage)}
-                    >
-                        {item.label}
-                    </div>
-                );
-            })}
+            <AnimateSharedLayout>
+                {items.map((item) => {
+                    return (
+                        <Item
+                            key={item.stage}
+                            selected={item.stage === stage}
+                            enabled={enabledStagesSet.has(item.stage)}
+                            onClick={() => onStageClick && onStageClick(item.stage)}
+                        >
+                            {item.label}
+                        </Item>
+                    );
+                })}
+            </AnimateSharedLayout>
         </div>
     );
 });
+
+interface ItemProps {
+    children: string;
+    onClick?: () => void;
+    enabled?: boolean | undefined;
+    selected?: boolean | undefined;
+}
+
+const Item: React.VFC<ItemProps> = ({ children, onClick, enabled, selected }) => {
+    return (
+        <div
+            onClick={onClick}
+            className={accessClassName(
+                styles,
+                "item",
+                enabled ? "enabledItem" : "disabledItem"
+            )}
+        >
+            {children}
+            {selected && <Underline/>}
+        </div>
+    );
+};
+
+const Underline: React.VFC = () => {
+    return (
+        <motion.div
+            className={accessClassName(styles, "underline")}
+            layout={true}
+            layoutId="underline"
+            initial={false}
+            transition={{ duration: 0.5 }}
+        />
+    );
+};
