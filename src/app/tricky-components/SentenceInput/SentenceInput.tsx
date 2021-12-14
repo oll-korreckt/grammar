04cee4fa132fa3d18c9ed9ddbbec4776fb0401c2
ok <oll.korreckt@gmail.com>
2021-12-14 15:22:34 -0500

    Simplified SentenceInput implementation

--- src/app/tricky-components/SentenceInput/SentenceInput.tsx
+++ src/app/tricky-components/SentenceInput/SentenceInput.tsx
@@ -8,15 +8,21 @@ import { TextEditor } from "../TextEditor";
 import styles from "./_styles.scss";
 
 export interface SentenceInputProps {
-    children?: string | undefined;
-    onSubmit?: (value: string) => void;
+    initialValue?: string;
+    onStateChange?: (state: SentenceInputState) => void;
 }
+
+export interface SentenceInputState {
+    value: string;
+    errorState: ErrorState;
+}
+
+type ErrorState = "none" | "errors" | "calculating";
 
 interface State {
     input: string;
     errors: DecoratorRange[];
     lastChange?: "input" | "error";
-    deferredSubmit?: boolean;
 }
 
 type Action = {
@@ -25,37 +31,22 @@ type Action = {
 } | {
     type: "update errors";
     errors: DecoratorRange[];
-} | {
-    type: "activate deferral";
-} | {
-    type: "remove deferral";
 }
 
 function reducer(state: State, action: Action): State {
     switch (action.type) {
         case "update input":
             return {
-                ...state,
                 input: action.input,
                 errors: state.errors,
                 lastChange: "input"
             };
         case "update errors":
             return {
-                ...state,
                 input: state.input,
                 errors: action.errors,
                 lastChange: "error"
             };
-        case "activate deferral":
-            return {
-                ...state,
-                deferredSubmit: true
-            };
-        case "remove deferral":
-            const output = { ...state };
-            delete output.deferredSubmit;
-            return output;
     }
     throw "unhandled action";
 }
@@ -66,17 +57,24 @@ function extractInput(children: string | undefined): string {
 
 const errorDelay = 500;
 
-export const SentenceInput: React.VFC<SentenceInputProps> = ({ children, onSubmit }) => {
+export const SentenceInput: React.VFC<SentenceInputProps> = ({ initialValue, onStateChange }) => {
     const [state, dispatch] = useReducer(
         reducer,
         {
-            input: extractInput(children),
+            input: extractInput(initialValue),
             errors: []
         }
     );
     const editor = useMemo(() => withReact(createEditor()), []);
     const editorRef = useRef<HTMLDivElement>(null);
     const [selectedErr, setSelectedErr] = useState<string>();
+    const currentInputRef = useRef<string>();
+
+    function invokeOnStateChange(newState: SentenceInputState): void {
+        if (onStateChange) {
+            onStateChange(newState);
+        }
+    }
 
     function setCursor(errKey: string): void {
         if (editorRef.current === null) {
@@ -91,12 +89,6 @@ export const SentenceInput: React.VFC<SentenceInputProps> = ({ children, onSubmi
         }
     }
 
-    function invokeSubmit(): void {
-        if (onSubmit) {
-            onSubmit(state.input);
-        }
-    }
-
     const errListItems: ErrorListItem[] = state.errors.map(({ key, message, anchor }) => {
         const lineNum = anchor.path[0] + 1;
         const colNum = anchor.offset + 1;
@@ -106,15 +98,6 @@ export const SentenceInput: React.VFC<SentenceInputProps> = ({ children, onSubmi
         };
     });
 
-    if (state.deferredSubmit && state.lastChange === "error") {
-        if (state.errors.length === 0) {
-            invokeSubmit();
-        } else {
-            // errors are present so do nothing
-            dispatch({ type: "remove deferral" });
-        }
-    }
-
     return (
         <div className={accessClassName(styles, "container")}>
             <div className={accessClassName(styles, "editorContainer")}>
@@ -122,6 +105,13 @@ export const SentenceInput: React.VFC<SentenceInputProps> = ({ children, onSubmi
                     editor={editor}
                     editorRef={editorRef}
                     onErrorChange={(newErrors) => {
+                        const input: string = currentInputRef.current !== undefined
+                            ? currentInputRef.current
+                            : state.input;
+                        invokeOnStateChange({
+                            value: input,
+                            errorState: newErrors.length === 0 ? "none" : "errors"
+                        });
                         dispatch({
                             type: "update errors",
                             errors: newErrors
@@ -129,6 +119,11 @@ export const SentenceInput: React.VFC<SentenceInputProps> = ({ children, onSubmi
                     }}
                     className={accessClassName(styles, "editor")}
                     onInputChange={(newInput) => {
+                        invokeOnStateChange({
+                            value: newInput,
+                            errorState: "calculating"
+                        });
+                        currentInputRef.current = newInput;
                         dispatch({
                             type: "update input",
                             input: newInput
@@ -149,19 +144,6 @@ export const SentenceInput: React.VFC<SentenceInputProps> = ({ children, onSubmi
                     {errListItems}
                 </ExtendedErrorList>
             </div>
-            <button
-                disabled={state.errors.length > 0}
-                style={{ padding: "15px", width: "100px" }}
-                onClick={() => {
-                    if (state.lastChange === "input") {
-                        dispatch({ type: "activate deferral" });
-                    } else {
-                        invokeSubmit();
-                    }
-                }}
-            >
-                Submit
-            </button>
         </div>
     );
 };
