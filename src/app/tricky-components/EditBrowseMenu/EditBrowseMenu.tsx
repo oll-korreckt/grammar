28efb091717b13650b0083e73ff555adb41d1f04
ok <oll.korreckt@gmail.com>
2022-01-14 11:47:45 -0500

    Created properties to follow convention of other menu components

--- src/app/tricky-components/EditBrowseMenu/EditBrowseMenu.tsx
+++ src/app/tricky-components/EditBrowseMenu/EditBrowseMenu.tsx
@@ -2,8 +2,12 @@ import { accessClassName } from "@app/utils";
 import React from "react";
 import styles from "./_styles.scss";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface EditBrowseMenuProps {
+}
 
-export const EditBrowseMenu: React.VFC = () => {
+
+export const EditBrowseMenu: React.VFC<EditBrowseMenuProps> = () => {
     return (
         <span className={accessClassName(styles, "editBrowseMenu")}>
             Select Element to Edit
