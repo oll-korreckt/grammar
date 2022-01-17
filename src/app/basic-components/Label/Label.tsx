8c686a45691238b25733d902e8a763bdbf3ddf88
ok <oll.korreckt@gmail.com>
2022-01-17 11:42:35 -0500

    Created initial version of Label to replace Word, WordLabel, etc.

---
+++ src/app/basic-components/Label/Label.tsx
@@ -0,0 +1,54 @@
+import { accessClassName, Colors } from "@app/utils";
+import { makeRefComponent } from "@app/utils/hoc";
+import React from "react";
+import styles from "./_styles.scss";
+
+export interface LabelProps {
+    children: string | string[];
+    color?: Colors;
+    fade?: boolean | undefined;
+    header?: string;
+    onClick?: () => void;
+}
+
+function cancelHighlightDoubleClick(ev: React.MouseEvent): void {
+    if (ev.detail > 1) {
+        ev.preventDefault();
+    }
+}
+
+export const Label = makeRefComponent<HTMLSpanElement, LabelProps>("Label", ({ children, color, fade, header, onClick }, ref) => {
+    const defChildren: string[] = Array.isArray(children) ? children : [children];
+    if (defChildren.length === 0) {
+        throw "Cannot have empty label";
+    }
+    const classes = ["label"];
+    if (color !== undefined) {
+        classes.push(fade ? `${color}Fade` : color);
+    }
+    if (onClick) {
+        classes.push("pointerCursor");
+    }
+
+    return (
+        <span
+            ref={ref}
+            className={accessClassName(styles, ...classes)}
+            onMouseDown={cancelHighlightDoubleClick}
+            onClick={() => onClick && onClick()}
+        >
+            {header &&
+                <span className={accessClassName(styles, "header")}>
+                    {header}
+                </span>
+            }
+            {defChildren.map((child, index) => {
+                return (
+                    <span key={index}>
+                        {child}
+                    </span>
+                );
+            })}
+        </span>
+    );
+});
