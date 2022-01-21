ebda7bb1279b4f5426b3f0e257b5b610019381b7
ok <oll.korreckt@gmail.com>
2022-01-21 11:37:30 -0500

    Not used

--- src/domain/language/__tests__/element-id.tests.ts
+++
@@ -1,9 +0,0 @@
-import { assert } from "chai";
-import { ElementId } from "../element-id";
-
-describe("createWordId", () => {
-    test("works", () => {
-        const result = ElementId.createWordId(4);
-        assert.equal(result, "1.4");
-    });
-});
