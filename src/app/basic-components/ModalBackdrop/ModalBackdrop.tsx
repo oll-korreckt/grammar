b5a5a0f8587ac4c89feb8deaa4117f1476af1171
ok <oll.korreckt@gmail.com>
2022-01-25 12:46:50 -0500

    Fixed bug where event was firing when children are clicked.

--- src/app/basic-components/ModalBackdrop/ModalBackdrop.tsx
+++ src/app/basic-components/ModalBackdrop/ModalBackdrop.tsx
@@ -1,6 +1,6 @@
 import { accessClassName } from "@app/utils";
-import { makeRefComponent } from "@app/utils/hoc";
-import React from "react";
+import { makeRefComponent, mergeRefs } from "@app/utils/hoc";
+import React, { useRef } from "react";
 import styles from "./_styles.scss";
 
 export interface ModalBackdropProps {
@@ -9,11 +9,19 @@ export interface ModalBackdropProps {
 }
 
 export const ModalBackdrop = makeRefComponent<HTMLDivElement, ModalBackdropProps>("ModalBackdrop", ({ onClick, children }, ref) => {
+    const refObj = useRef<HTMLDivElement>(null);
+    const mergeRef = mergeRefs(ref, refObj);
+
     return (
         <div
-            ref={ref}
+            ref={mergeRef}
             className={accessClassName(styles, "modalBackdrop")}
-            onClick={() => onClick && onClick()}
+            onClick={(e) => {
+                if (onClick !== undefined
+                    && e.target === refObj.current) {
+                    onClick();
+                }
+            }}
         >
             {children}
         </div>
