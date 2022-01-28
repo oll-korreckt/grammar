1d4c0f6d6583b0db9df3d4ce4031c8323265a6c8
ok <oll.korreckt@gmail.com>
2022-01-28 10:06:29 -0500

    Revised MessageBox to make it more customizable.

--- src/app/basic-components/MessageBox/MessageBox.tsx
+++ src/app/basic-components/MessageBox/MessageBox.tsx
@@ -4,22 +4,61 @@ import React from "react";
 import styles from "./_styles.scss";
 
 export interface MessageBoxProps {
-    type: MessageBoxType;
+    buttons: MessageBoxButton[];
     children: string;
-    onResponse?: (response: MessageBoxResponse) => void;
+    onButtonClick?: (text: string) => void;
 }
 
-export type MessageBoxType = "yes no";
-export type MessageBoxResponse =
-    | "yes"
-    | "no";
+export interface MessageBoxButton {
+    text: string;
+    alignment: "left" | "right";
+}
 
-export const MessageBox = makeRefComponent<HTMLDivElement, MessageBoxProps>("MessageBox", ({ type, children, onResponse }, ref) => {
-    function invokeResponse(response: MessageBoxResponse): void {
-        if (onResponse) {
-            onResponse(response);
+interface SortedMessageBoxButtons {
+    leftButtons?: MessageBoxButton[];
+    rightButtons?: MessageBoxButton[];
+}
+
+function sortButtons(buttons: MessageBoxButton[]): SortedMessageBoxButtons {
+    if (buttons.length === 0) {
+        throw "No buttons provided";
+    } else if (buttons.length > 3) {
+        throw "More than 3 buttons provided";
+    }
+    const uniqueButtons = new Set(buttons.map(({ text }) => text));
+    if (uniqueButtons.size !== buttons.length) {
+        throw "Duplicate buttons provided";
+    }
+    const leftButtons: MessageBoxButton[] = [];
+    const rightButtons: MessageBoxButton[] = [];
+    buttons.forEach((button) => {
+        if (button.alignment === "left") {
+            leftButtons.push(button);
+        } else {
+            rightButtons.push(button);
+        }
+    });
+    const output: SortedMessageBoxButtons = {};
+    if (leftButtons.length > 0) {
+        output.leftButtons = leftButtons;
+    }
+    if (rightButtons.length > 0) {
+        output.rightButtons = rightButtons;
+    }
+    return output;
+}
+
+export const MessageBox = makeRefComponent<HTMLDivElement, MessageBoxProps>("MessageBox", ({ buttons, children, onButtonClick }, ref) => {
+    const { leftButtons, rightButtons } = sortButtons(buttons);
+    function invokeOnButtonClick(text: string): void {
+        if (onButtonClick) {
+            onButtonClick(text);
         }
     }
+
+    const buttonSectionClass = leftButtons !== undefined && rightButtons !== undefined
+        ? "twoButtonSections"
+        : "oneButtonSection";
 
     return (
         <div
@@ -29,22 +68,39 @@ export const MessageBox = makeRefComponent<HTMLDivElement, MessageBoxProps>("Mes
             <div className={accessClassName(styles, "messageSection")}>
                 {children}
             </div>
-            <div className={accessClassName(styles, "buttonSection")}>
-                {type === "yes no" &&
-                    <button
-                        className={accessClassName(styles, "button")}
-                        onClick={() => invokeResponse("yes")}
+            <div className={accessClassName(styles, buttonSectionClass)}>
+                {leftButtons &&
+                    <div
+                        key="left"
+                        className={accessClassName(styles, "leftButtonSection")}
                     >
-                        Yes
-                    </button>
+                        {leftButtons.map(({ text }) => (
+                            <button
+                                key={text}
+                                className={accessClassName(styles, "button")}
+                                onClick={() => invokeOnButtonClick(text)}
+                            >
+                                {text}
+                            </button>
+                        ))}
+                    </div>
+
                 }
-                {type === "yes no" &&
-                    <button
-                        className={accessClassName(styles, "button")}
-                        onClick={() => invokeResponse("no")}
+                {rightButtons &&
+                    <div
+                        key="right"
+                        className={accessClassName(styles, "rightButtonSection")}
                     >
-                        No
-                    </button>
+                        {rightButtons.map(({ text }) => (
+                            <button
+                                key={text}
+                                className={accessClassName(styles, "button")}
+                                onClick={() =>invokeOnButtonClick(text)}
+                            >
+                                {text}
+                            </button>
+                        ))}
+                    </div>
                 }
             </div>
         </div>
