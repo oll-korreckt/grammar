78741f71245682bb47c3e0d697459efc9ca5769b
ok <oll.korreckt@gmail.com>
2022-02-08 09:31:35 -0500

    Got pages folder setup inside src directory

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [
+            path.resolve(__dirname, "src/app")
+        ]
+    }
 };
 
 module.exports = nextConfig;
