4760d76b317e10c5b7832c79e711ce8903c8331f
ok <oll.korreckt@gmail.com>
2022-03-25 10:04:23 -0400

    Minor changes to NavBar

--- src/utils/NavBar/NavBar.tsx
+++ src/utils/NavBar/NavBar.tsx
@@ -2,27 +2,66 @@ import React from "react";
 import Link from "next/link";
 import styles from "./_styles.module.scss";
 import { accessClassName } from "@app/utils";
+import { FaHome, FaTree, FaQuestion, FaInfoCircle } from "react-icons/fa";
+import { IconType } from "react-icons";
 
 export const NavBar: React.VFC = () => {
     return (
         <nav className={accessClassName(styles, "navBar")}>
             <ul className={accessClassName(styles, "list")}>
                 <li>
-                    <Link href="/">
+                    <NavBarItem
+                        href="/"
+                        icon={FaHome}
+                    >
                         Home
-                    </Link>
+                    </NavBarItem>
                 </li>
                 <li>
-                    <Link href="/app">
+                    <NavBarItem
+                        href="/app"
+                        icon={FaTree}
+                    >
                         App
-                    </Link>
+                    </NavBarItem>
                 </li>
                 <li>
-                    <Link href="/element">
+                    <NavBarItem
+                        href="/element"
+                        icon={FaQuestion}
+                    >
                         Elements
-                    </Link>
+                    </NavBarItem>
+                </li>
+                <li>
+                    <NavBarItem
+                        href="/"
+                        icon={FaInfoCircle}
+                    >
+                        About
+                    </NavBarItem>
                 </li>
             </ul>
         </nav>
+    );
+};
+
+interface NavBarItemProps {
+    icon: IconType;
+    href: string;
+    children: string;
+}
+
+const NavBarItem: React.VFC<NavBarItemProps> = ({ icon, children, href }) => {
+    const Icon = icon;
+    return (
+        <Link href={href}>
+            <div className={accessClassName(styles, "navBarItem")}>
+                <Icon className={accessClassName(styles, "navBarIcon")}/>
+                <div className={accessClassName(styles, "navBarChildren")}>
+                    {children}
+                </div>
+            </div>
+        </Link>
     );
 };
