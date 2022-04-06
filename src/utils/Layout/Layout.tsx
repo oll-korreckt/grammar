19202cc51320a649fc806c6ec1ad16a2612bc91e
ok <oll.korreckt@gmail.com>
2022-04-06 15:06:10 -0400

    Had to add to use react query in pages

--- src/utils/Layout/Layout.tsx
+++ src/utils/Layout/Layout.tsx
@@ -1,15 +1,20 @@
 import { accessClassName } from "@app/utils";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { NavBar } from "utils/NavBar";
 import styles from "./_styles.module.scss";
+
+const queryClient = new QueryClient();
 
 export const Layout: React.FC = ({ children }) => {
     return (
         <>
-            <NavBar/>
-            <main className={accessClassName(styles, "main")}>
-                {children}
-            </main>
+            <QueryClientProvider client={queryClient}>
+                <NavBar/>
+                <main className={accessClassName(styles, "main")}>
+                    {children}
+                </main>
+            </QueryClientProvider>
         </>
     );
 };
