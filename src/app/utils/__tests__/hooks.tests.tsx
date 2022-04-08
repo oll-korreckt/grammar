2f279aaab393221f49db3727926b1faab9de1b0b
ok <oll.korreckt@gmail.com>
2022-04-08 11:27:49 -0400

    Created hook for handling interactions with localStorage

--- src/app/utils/__tests__/hooks.tests.tsx
+++ src/app/utils/__tests__/hooks.tests.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
-import { useClientRect, useOutsideClick } from "../hooks";
+import { render, screen, fireEvent, RenderResult } from "@testing-library/react";
+import { createLocalStorageHook, useClientRect, useOutsideClick } from "../hooks";
+import { assert } from "chai";
 
 const MockComponent: React.VFC<{ onClick: () => void; }> = ({ onClick }) => {
     const ref = useOutsideClick<HTMLDivElement>(onClick);
@@ -17,6 +18,27 @@ const CRComponent: React.VFC = () => {
     const text = rect === undefined ? undefined : `top: ${rect.top} left: ${rect.left}`;
     return <div data-testid="yo" ref={ref}>{text}</div>;
 };
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function serialize(value: Person): string {
+    return JSON.stringify(value);
+}
+
+function deserialize(value: string): Person {
+    return JSON.parse(value);
+}
+
+const personSerializer = { serialize, deserialize };
+
+const useStringLocalStorage = createLocalStorageHook("string");
+const useDefStringLocalStorage = createLocalStorageHook("defString", "Default Value");
+const useTypedLocalStorage = createLocalStorageHook<Person>("typed", personSerializer);
+const useDefTypedLocalStorage = createLocalStorageHook<Person>("defTyped", personSerializer, { name: "Bob", age: 42 });
+
 
 describe("hooks", () => {
     describe("useOutsideClick", () => {
@@ -41,5 +63,269 @@ describe("hooks", () => {
     test("useClientRect", () => {
         const result = render(<CRComponent />).getByTestId("yo");
         expect(result.textContent).not.toBeUndefined();
+    });
+
+    describe("createLocalStorageHook", () => {
+        interface TestResultUtils {
+            updateClick: () => void;
+            getValueText: () => string | null;
+            resetClick: () => void;
+            clearClick: () => void;
+        }
+
+        function getTestResultUtils(result: RenderResult): TestResultUtils {
+            return {
+                updateClick: () => fireEvent.click(result.getByTestId("update")),
+                getValueText: () => result.getByTestId("value").textContent,
+                resetClick: () => fireEvent.click(result.getByTestId("reset")),
+                clearClick: () => fireEvent.click(result.getByTestId("clear"))
+            };
+        }
+
+        beforeEach(() => localStorage.clear());
+
+        describe("string", () => {
+            const LocalStorageComponent: React.VFC = () => {
+                const { value, update, clear, reset } = useStringLocalStorage();
+                return (
+                    <>
+                        <p data-testid="value">
+                            {value}
+                        </p>
+                        <button
+                            data-testid="update"
+                            onClick={() => {
+                                if (value === undefined) {
+                                    update("1");
+                                    return;
+                                }
+                                let numVal = parseInt(value);
+                                numVal++;
+                                update(numVal.toString());
+                            }}
+                        >
+                            Update
+                        </button>
+                        <button
+                            data-testid="reset"
+                            onClick={reset}
+                        >
+                            Reset
+                        </button>
+                        <button
+                            data-testid="clear"
+                            onClick={clear}
+                        >
+                            Clear
+                        </button>
+                    </>
+                );
+            };
+
+            const getLocalStorage = () => localStorage.getItem("string");
+
+            test("run", () => {
+                const result = render(<LocalStorageComponent/>);
+                const resultUtils = getTestResultUtils(result);
+
+                assert.strictEqual(resultUtils.getValueText(), "");
+                assert.strictEqual(getLocalStorage(), null);
+
+                resultUtils.updateClick();
+                assert.strictEqual(resultUtils.getValueText(), "1");
+                assert.strictEqual(getLocalStorage(), "1");
+
+                resultUtils.updateClick();
+                assert.strictEqual(resultUtils.getValueText(), "2");
+                assert.strictEqual(getLocalStorage(), "2");
+
+                resultUtils.resetClick();
+                assert.strictEqual(resultUtils.getValueText(), "");
+                assert.strictEqual(getLocalStorage(), null);
+
+                resultUtils.updateClick();
+                resultUtils.clearClick();
+                assert.strictEqual(resultUtils.getValueText(), "");
+                assert.strictEqual(getLocalStorage(), null);
+            });
+        });
+
+        describe("defString", () => {
+            const LocalStorageComponent: React.VFC = () => {
+                const { value, update, clear, reset } = useDefStringLocalStorage();
+                return (
+                    <>
+                        <p data-testid="value">
+                            {value}
+                        </p>
+                        <button
+                            data-testid="update"
+                            onClick={() => {
+                                if (value === "Default Value") {
+                                    update("1");
+                                    return;
+                                }
+                                let numVal = parseInt(value);
+                                numVal++;
+                                update(numVal.toString());
+                            }}
+                        >
+                            Update
+                        </button>
+                        <button
+                            data-testid="reset"
+                            onClick={reset}
+                        >
+                            Reset
+                        </button>
+                        <button
+                            data-testid="clear"
+                            onClick={clear}
+                        >
+                            Clear
+                        </button>
+                    </>
+                );
+            };
+
+            const getLocalStorage = () => localStorage.getItem("defString");
+
+            test("run", () => {
+                const result = render(<LocalStorageComponent/>);
+                const resultUtils = getTestResultUtils(result);
+
+                assert.strictEqual(resultUtils.getValueText(), "Default Value");
+                assert.strictEqual(getLocalStorage(), null);
+
+                resultUtils.updateClick();
+                assert.strictEqual(resultUtils.getValueText(), "1");
+                assert.strictEqual(getLocalStorage(), "1");
+
+                resultUtils.updateClick();
+                assert.strictEqual(resultUtils.getValueText(), "2");
+                assert.strictEqual(getLocalStorage(), "2");
+
+                resultUtils.resetClick();
+                assert.strictEqual(resultUtils.getValueText(), "Default Value");
+                assert.strictEqual(getLocalStorage(), "Default Value");
+
+                resultUtils.clearClick();
+                assert.strictEqual(resultUtils.getValueText(), "Default Value");
+                assert.strictEqual(getLocalStorage(), null);
+            });
+        });
+
+        describe("typed", () => {
+            const LocalStorageComponent: React.VFC = () => {
+                const { value, update, reset, clear } = useTypedLocalStorage();
+                return (
+                    <>
+                        <p data-testid="value">
+                            {value
+                                ? `Name: ${value.name}, Age: ${value.age}`
+                                : "No Value"
+                            }
+                        </p>
+                        <button
+                            data-testid="update"
+                            onClick={() => {
+                                if (value === undefined) {
+                                    update({ name: "Bob", age: 42 });
+                                }
+                            }}
+                        >
+                            Update
+                        </button>
+                        <button
+                            data-testid="reset"
+                            onClick={reset}
+                        >
+                            Reset
+                        </button>
+                        <button
+                            data-testid="clear"
+                            onClick={clear}
+                        >
+                            Clear
+                        </button>
+                    </>
+                );
+            };
+
+            const getLocalStorage = () => localStorage.getItem("typed");
+
+            test("run", () => {
+                const result = render(<LocalStorageComponent/>);
+                const resultUtils = getTestResultUtils(result);
+
+                assert.strictEqual(resultUtils.getValueText(), "No Value");
+                assert.strictEqual(getLocalStorage(), null);
+
+                resultUtils.updateClick();
+                assert.strictEqual(resultUtils.getValueText(), "Name: Bob, Age: 42");
+                assert.strictEqual(getLocalStorage(), JSON.stringify({ name: "Bob", age: 42 }));
+
+                resultUtils.resetClick();
+                assert.strictEqual(resultUtils.getValueText(), "No Value");
+                assert.strictEqual(getLocalStorage(), null);
+
+                resultUtils.clearClick();
+                assert.strictEqual(resultUtils.getValueText(), "No Value");
+                assert.strictEqual(getLocalStorage(), null);
+            });
+        });
+
+        describe("defTyped", () => {
+            const LocalStorageComponent: React.VFC = () => {
+                const { value, update, reset, clear } = useDefTypedLocalStorage();
+                return (
+                    <>
+                        <p data-testid="value">
+                            {`Name: ${value.name}, Age: ${value.age}`}
+                        </p>
+                        <button
+                            data-testid="update"
+                            onClick={() => update({ name: "Tim", age: 100 })}
+                        >
+                            Update
+                        </button>
+                        <button
+                            data-testid="reset"
+                            onClick={reset}
+                        >
+                            Reset
+                        </button>
+                        <button
+                            data-testid="clear"
+                            onClick={clear}
+                        >
+                            Clear
+                        </button>
+                    </>
+                );
+            };
+
+            const getLocalStorage = () => localStorage.getItem("defTyped");
+
+            test("run", () => {
+                const result = render(<LocalStorageComponent/>);
+                const resultUtils = getTestResultUtils(result);
+
+                assert.strictEqual(resultUtils.getValueText(), "Name: Bob, Age: 42");
+                assert.strictEqual(getLocalStorage(), null);
+
+                resultUtils.updateClick();
+                assert.strictEqual(resultUtils.getValueText(), "Name: Tim, Age: 100");
+                assert.strictEqual(getLocalStorage(), JSON.stringify({ name: "Tim", age: 100 }));
+
+                resultUtils.resetClick();
+                assert.strictEqual(resultUtils.getValueText(), "Name: Bob, Age: 42");
+                assert.strictEqual(getLocalStorage(), JSON.stringify({ name: "Bob", age: 42 }));
+
+                resultUtils.clearClick();
+                assert.strictEqual(resultUtils.getValueText(), "Name: Bob, Age: 42");
+                assert.strictEqual(getLocalStorage(), null);
+            });
+        });
     });
 });
--- src/app/utils/hooks.ts
+++ src/app/utils/hooks.ts
@@ -1,4 +1,5 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import { SimpleObject } from "@lib/utils";
+import React, { useCallback, useEffect, useReducer, useRef, useState } from "react";
 import { Rect } from "./types";
 
 export function useOutsideClick<TRef extends HTMLElement>(callback: () => void): React.MutableRefObject<TRef | null> {
@@ -56,4 +57,220 @@ export function useClientSide(): boolean {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return render;
+}
+
+export interface LocalStorage {
+    clear: () => void;
+    reset: () => void;
+    value?: string;
+    update: (value: string) => void;
+}
+
+export interface DefinedLocalStorage extends Omit<LocalStorage, "value"> {
+    value: string;
+}
+
+export interface TypedLocalStorage<T> extends Pick<LocalStorage, "clear" | "reset"> {
+    value?: T;
+    update: (value: T) => void;
+}
+
+export interface DefinedTypedLocalStorage<T> extends Omit<TypedLocalStorage<T>, "value"> {
+    value: T;
+}
+
+export interface LocalStorageSerializer<T> {
+    serialize: (value: T) => string;
+    deserialize: (value: string) => T;
+}
+
+export type LocalStorageHook = () => LocalStorage;
+export type DefinedLocalStorageHook = () => DefinedLocalStorage;
+export type TypedLocalStorageHook<T> = () => TypedLocalStorage<T>;
+export type DefinedTypedLocalStorageHook<T> = () => DefinedTypedLocalStorage<T>;
+
+interface LocalStorageState<T> {
+    command: "update" | "clear" | "reset" | "init";
+    value?: T;
+    default?: T;
+}
+
+type LocalStorageAction<T> = {
+    type: "update";
+    value: T;
+} | {
+    type: "clear";
+} | {
+    type: "reset";
+}
+
+function localStorageReducer<T>(state: LocalStorageState<T>, action: LocalStorageAction<T>): LocalStorageState<T> {
+    let output: LocalStorageState<T>;
+    switch (action.type) {
+        case "update":
+            output = {
+                ...state,
+                command: "update",
+                value: action.value
+            };
+            break;
+        case "clear":
+            output = {
+                ...state,
+                command: "clear",
+                value: state.default
+            };
+            break;
+        case "reset":
+            output = {
+                ...state,
+                command: "reset",
+                value: state.default
+            };
+            break;
+    }
+    return SimpleObject.clean(output) as LocalStorageState<T>;
+}
+
+function localStorageInitializer<T>(config: FunctionConfig<T>): LocalStorageState<T> {
+    const output: LocalStorageState<T> = { command: "init" };
+    if (config.type === "defString" || config.type === "defTyped") {
+        output.default = config.defaultValue as any;
+    }
+    const rawStoredValue = localStorage.getItem(config.key);
+    if (rawStoredValue === null) {
+        // not in localStorage
+        output.value = output.default;
+        return SimpleObject.clean(output) as LocalStorageState<T>;
+    }
+    if (config.type === "typed" || config.type === "defTyped") {
+        const { deserialize } = config.serializer;
+        output.value = deserialize(rawStoredValue);
+    } else {
+        output.value = rawStoredValue as any;
+    }
+    return SimpleObject.clean(output) as LocalStorageState<T>;
+}
+
+function storeValue<T>(config: FunctionConfig<T>, value?: T | string): void {
+    if (value === undefined) {
+        localStorage.removeItem(config.key);
+        return;
+    }
+    const storeValueString = config.type === "typed" || config.type === "defTyped"
+        ? config.serializer.serialize(value as T)
+        : value as string;
+    if (typeof storeValueString !== "string") {
+        throw "cannot serializer non-strings in localStorage";
+    }
+    localStorage.setItem(config.key, storeValueString);
+}
+
+type FunctionConfig<T> = {
+    type: "string";
+    key: string;
+} | {
+    type: "defString";
+    key: string;
+    defaultValue: string;
+} | {
+    type: "typed";
+    key: string;
+    serializer: LocalStorageSerializer<T>;
+} | {
+    type: "defTyped";
+    key: string;
+    serializer: LocalStorageSerializer<T>;
+    defaultValue: T;
+}
+
+function getFunctionConfig<T>(key: string, defaultValueOrSerializer?: string | LocalStorageSerializer<T>, defaultValue?: T): FunctionConfig<T> {
+    const arg1 = 1 << 0;
+    const arg2 = 1 << 1;
+    let state = 0;
+    if (defaultValueOrSerializer !== undefined) {
+        state |= arg1;
+    }
+    if (defaultValue !== undefined) {
+        state |= arg2;
+    }
+    switch (state) {
+        case 0: {
+            return { type: "string", key };
+        }
+        case arg1: {
+            return typeof defaultValueOrSerializer === "string"
+                ?
+                {
+                    type: "defString",
+                    defaultValue: defaultValueOrSerializer,
+                    key
+                }
+                :
+                {
+                    type: "typed",
+                    serializer: defaultValueOrSerializer as LocalStorageSerializer<T>,
+                    key
+                };
+        }
+        case arg1 | arg2: {
+            return {
+                type: "defTyped",
+                serializer: defaultValueOrSerializer as LocalStorageSerializer<T>,
+                defaultValue: defaultValue as T,
+                key
+            };
+        }
+        default: {
+            throw "Unexpected function configuration";
+        }
+    }
+}
+
+export function createLocalStorageHook(key: string): LocalStorageHook
+export function createLocalStorageHook(key: string, defaultValue: string): DefinedLocalStorageHook
+export function createLocalStorageHook<T>(key: string, serializer: LocalStorageSerializer<T>): TypedLocalStorageHook<T>;
+export function createLocalStorageHook<T>(key: string, serializer: LocalStorageSerializer<T>, defaultValue: T): DefinedTypedLocalStorageHook<T>
+export function createLocalStorageHook<T>(key: string, defaultValueOrSerializer?: string | LocalStorageSerializer<T>, defaultValue?: T): LocalStorageHook | DefinedLocalStorageHook | TypedLocalStorageHook<T> | DefinedTypedLocalStorageHook<T> {
+
+    const config = getFunctionConfig(key, defaultValueOrSerializer, defaultValue);
+
+    const useLocalStorage: LocalStorageHook | TypedLocalStorageHook<T> = () => {
+        const [state, dispatch] = useReducer(localStorageReducer, config, localStorageInitializer);
+
+        useEffect(() => {
+            const { command } = state;
+            switch (command) {
+                case "init": {
+                    // do nothing
+                    break;
+                }
+                case "clear": {
+                    localStorage.removeItem(config.key);
+                    break;
+                }
+                case "reset": {
+                    if (config.type === "defString" || config.type === "defTyped") {
+                        storeValue(config, config.defaultValue);
+                    } else {
+                        localStorage.removeItem(config.key);
+                    }
+                    break;
+                }
+                case "update": {
+                    storeValue(config as any, state.value);
+                    break;
+                }
+            }
+        }, [state]);
+
+        return SimpleObject.clean({
+            clear: () => dispatch({ type: "clear" }),
+            reset: () => dispatch({ type: "reset" }),
+            update: (value: any) => dispatch({ type: "update", value }),
+            value: state.value
+        }) as any;
+    };
+
+    return useLocalStorage;
 }
