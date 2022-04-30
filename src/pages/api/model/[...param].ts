13661918b46da928f28a6572a01233b9cb384265
ok <oll.korreckt@gmail.com>
2022-04-30 11:39:08 -0400

    Added endpoint for getting just models of certain type

--- src/pages/api/model/[...param].ts
+++ src/pages/api/model/[...param].ts
@@ -1,59 +1,79 @@
 import { NextApiResponse } from "next";
-import { ElementPage } from "@utils/element";
+import { ElementPage, ElementPageId } from "@utils/element";
 import nodepath from "path";
 import { ElementModelAddress, ModelLoader } from "@utils/model/io";
 import { ApiRequestHandler, createApiRequestHandler, ErrorMessage, sendError } from "@utils/api";
 import { Model } from "@utils/model";
 
-function isError(value: ElementModelAddress | ErrorMessage): value is ErrorMessage {
-    return "status" in value;
+function isError(value: ElementModelAddress | ElementPageId | ErrorMessage): value is ErrorMessage {
+    return typeof value === "object" && "status" in value;
 }
 
 function createNotFound(page: string, model: string): string {
     return `Unable to find model for '${page}.${model}'`;
 }
 
-function getRouteData(path: string | string[] | undefined): ElementModelAddress | ErrorMessage {
+function getRouteData(path: string | string[] | undefined): ElementModelAddress | ElementPageId | ErrorMessage {
     if (path === undefined) {
         return { status: 400, msg: "Invalid query" };
     }
     const pathArray: string[] = typeof path === "string" ? [path] : path;
-    if (pathArray.length !== 2) {
-        return {
-            status: 400,
-            msg: "Invalid query"
-        };
+    switch (pathArray.length) {
+        case 1: {
+            const [page] = pathArray;
+            return ElementPage.isPageId(page)
+                ? page
+                : { status: 400, msg: `'${page}' is not a page` };
+        }
+        case 2: {
+            const [page, model] = pathArray;
+            if (!ElementPage.isPageId(page)) {
+                return {
+                    status: 400,
+                    msg: createNotFound(page, model)
+                };
+            }
+            return { page, name: model };
+        }
+        default: {
+            return {
+                status: 400,
+                msg: "Invalid query"
+            };
+        }
     }
-    const [page, model] = pathArray;
-    if (!ElementPage.isPageId(page)) {
-        return {
-            status: 400,
-            msg: createNotFound(page, model)
-        };
-    }
-    return { page, name: model };
 }
 
 const loader = ModelLoader.createLoader(nodepath.resolve("src", "data", "element"));
 
-async function handleGet(address: ElementModelAddress, res: NextApiResponse): Promise<void> {
-    const result = await loader.getModel(address);
-    switch (result) {
-        case "no model":
-        case "error":
-            const err: ErrorMessage = {
-                status: 404,
-                msg: `No model for ${address.page}: ${address.name}`
-            };
-            sendError(res, err);
-            break;
-        default:
+async function handleGet(address: ElementModelAddress | ElementPageId, res: NextApiResponse): Promise<void> {
+    if (typeof address === "string") {
+        const result = await loader.getModelAddresses(address);
+        if (result === "error") {
+            sendError(res, 500);
+            return;
+        } else {
             res.status(200).json(result);
-            break;
+        }
+    } else {
+        const result = await loader.getModel(address);
+        switch (result) {
+            case "no model":
+            case "error":
+                const err: ErrorMessage = {
+                    status: 404,
+                    msg: `No model for ${address.page}: ${address.name}`
+                };
+                sendError(res, err);
+                break;
+            default:
+                res.status(200).json(result);
+                break;
+        }
     }
 }
 
-type Handler = (model: ElementModelAddress, res: NextApiResponse) => Promise<void>;
+type Handler = (model: ElementModelAddress | ElementPageId, res: NextApiResponse) => Promise<void>;
 
 function _extendHandler(handler: Handler): ApiRequestHandler {
     return async (req, res) => {
@@ -67,7 +87,7 @@ function _extendHandler(handler: Handler): ApiRequestHandler {
     };
 }
 
-type HandlerWithBody<TBody> = (model: ElementModelAddress, body: TBody, res: NextApiResponse) => Promise<void>;
+type HandlerWithBody<TBody> = (model: ElementModelAddress | ElementPageId, body: TBody, res: NextApiResponse) => Promise<void>;
 
 function _extendHandlerWithBody<TBody>(handler: HandlerWithBody<TBody>): ApiRequestHandler {
     return async (req, res) => {
@@ -81,18 +101,28 @@ function _extendHandlerWithBody<TBody>(handler: HandlerWithBody<TBody>): ApiRequ
     };
 }
 
-async function handlePut(address: ElementModelAddress, model: Model, res: NextApiResponse): Promise<void> {
+async function handlePut(address: ElementModelAddress | ElementPageId, model: Model, res: NextApiResponse): Promise<void> {
+    if (typeof address === "string") {
+        sendError(res, 400);
+        return;
+    }
     const result = await loader.setModel(address, model);
     switch (result) {
         case "success":
             res.status(200).end();
             break;
+        case "invalid":
         case "error":
+            sendError(res, 400);
             break;
     }
 }
 
-async function handleDelete(address: ElementModelAddress, res: NextApiResponse): Promise<void> {
+async function handleDelete(address: ElementModelAddress | ElementPageId, res: NextApiResponse): Promise<void> {
+    if (typeof address === "string") {
+        sendError(res, 400);
+        return;
+    }
     const result = await loader.deleteModel(address);
     switch (result) {
         case "success":
@@ -106,7 +136,11 @@ async function handleDelete(address: ElementModelAddress, res: NextApiResponse):
     return;
 }
 
-async function handlePatch(address: ElementModelAddress, newAddress: ElementModelAddress, res: NextApiResponse): Promise<void> {
+async function handlePatch(address: ElementModelAddress | ElementPageId, newAddress: ElementModelAddress, res: NextApiResponse): Promise<void> {
+    if (typeof address === "string") {
+        sendError(res, 400);
+        return;
+    }
     const result = await loader.renameModel(address, newAddress);
     switch (result) {
         case "success":
