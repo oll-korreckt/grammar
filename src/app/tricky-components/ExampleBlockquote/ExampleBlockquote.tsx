80026c35095440707a8fe69dd2de8ef78f8dd6d9
ok <oll.korreckt@gmail.com>
2022-05-21 12:20:58 -0400

    Revised development mode check to check for address validity and model validity as well.

--- src/app/tricky-components/ExampleBlockquote/ExampleBlockquote.tsx
+++ src/app/tricky-components/ExampleBlockquote/ExampleBlockquote.tsx
@@ -1,99 +1,39 @@
 import { HTMLBlockquoteObject } from "@lib/utils";
-import { ElementPage, ElementPageType } from "@utils/element";
-import { ElementModelAddress } from "@utils/model";
-import { SERVER } from "config";
-import React, { useEffect, useReducer } from "react";
-import { useQuery } from "react-query";
+import React, { useState } from "react";
 import { ExampleBlockquoteClosed } from "./ExampleBlockquoteClosed";
 import { ExampleBlockquoteOpen } from "./ExampleBlockquoteOpen";
+import { createCheckHook } from "./utils";
 
 export interface ExampleBlockquoteProps {
     children: HTMLBlockquoteObject;
 }
 
-async function queryFn(): Promise<Set<string>> {
-    if (process.env.NODE_ENV !== "development") {
-        return new Set();
-    }
-    const response = await fetch(`${SERVER}/api/model`);
-    if (!response.ok) {
-        let errMsg = "error during query for all models";
-        try {
-            errMsg = await response.text();
-        } catch {
-        }
-        throw errMsg;
-    }
-    const output: ElementModelAddress[] = await response.json();
-    return new Set(output.map((address) => ElementModelAddress.toString(address)));
-}
-
-type State = {
-    type: "closed" | "open";
-} | {
-    type: "error";
-    msg?: string;
-}
-
-type Action = State;
-
-function reducer(state: State, action: Action): State {
-    return action;
-}
+const useCheckHook = createCheckHook();
 
 export const ExampleBlockquote: React.VFC<ExampleBlockquoteProps> = ({ children }) => {
-    const query = useQuery<Set<string>, string>(
-        ["ExampleBlockquote"],
-        queryFn,
-        { onError: (msg) => dispatch({ type: "error", msg }) }
-    );
-    const [state, dispatch] = useReducer(reducer, { type: "closed" });
+    const [state, setState] = useState<"open" | "closed">("closed");
+    const check = useCheckHook(children);
 
-    useEffect(() => {
-        if (children.custom === undefined) {
-            dispatch({
-                type: "error",
-                msg: "No custom property given"
-            });
-            return;
-        }
-        if (process.env.NODE_ENV === "development"
-            && query.status === "success") {
-            const [pageType, name] = children.custom.split(".");
-            const value = ElementModelAddress.toString({
-                page: ElementPage.typeToId(pageType as ElementPageType),
-                name: name
-            });
-            if (!query.data.has(value)) {
-                dispatch({
-                    type: "error",
-                    msg: `No model exists for '${children.custom}'`
-                });
-            }
-        }
-    }, [query.status, query.data, children.custom]);
-
-    switch (state.type) {
-        case "closed":
-            return (
-                <ExampleBlockquoteClosed onOpen={() => dispatch({ type: "open" })}>
-                    {children}
-                </ExampleBlockquoteClosed>
-            );
-        case "open":
-            return (
-                <ExampleBlockquoteOpen onExit={() => dispatch({ type: "closed" })}>
-                    {children}
-                </ExampleBlockquoteOpen>
-            );
-        case "error":
-            const errMsg = state.msg === undefined
-                ? "An error occurred"
-                : state.msg;
-            return (
-                <blockquote>
-                    {errMsg}
-                </blockquote>
-            );
+    if (check.type !== "ok") {
+        const content = check.type === "error"
+            ? check.msg
+            : "Running check";
+        return (
+            <blockquote>
+                {content}
+            </blockquote>
+        );
+    } else if (state === "closed") {
+        return (
+            <ExampleBlockquoteClosed onOpen={() => setState("open")}>
+                {children}
+            </ExampleBlockquoteClosed>
+        );
+    } else {
+        return (
+            <ExampleBlockquoteOpen onExit={() => setState("closed")}>
+                {children}
+            </ExampleBlockquoteOpen>
+        );
     }
 };
---
+++ src/app/tricky-components/ExampleBlockquote/utils.ts
@@ -0,0 +1,203 @@
+import { DiagramState } from "@app/utils";
+import { HTMLBlockquoteObject, HTMLContent, HTMLObject } from "@lib/utils";
+import { ElementPageId } from "@utils/element";
+import { ElementModelAddress, Model } from "@utils/model";
+import { SERVER } from "config";
+import { useEffect, useState } from "react";
+import { QueryFunctionContext, useQuery } from "react-query";
+
+export type CheckHookState = {
+    type: "ok";
+} | {
+    type: "checking";
+} | {
+    type: "error";
+    msg: string;
+}
+
+type CheckModelKeyBase = ["ExampleBlockquote", "Check"];
+type CheckModelKey =
+    | [...CheckModelKeyBase, "Invalid"]
+    | [...CheckModelKeyBase, ElementPageId, string];
+
+type InternalState = {
+    type: "check address";
+} | {
+    type: "check model";
+} | {
+    type: "done";
+} | {
+    type: "error";
+    msg: string;
+}
+
+async function availableQueryFn(): Promise<Set<string>> {
+    const response = await fetch(`${SERVER}/api/model`);
+    if (!response.ok) {
+        let errMsg = "Unable to fetch addresses";
+        try {
+            errMsg = await response.text();
+        } catch {
+        }
+        throw errMsg;
+    }
+    const addresses: ElementModelAddress[] = await response.json();
+    return new Set(addresses.map((address) => ElementModelAddress.toString(address)));
+}
+
+async function textQueryFn({ queryKey }: QueryFunctionContext<CheckModelKey>): Promise<string> {
+    if (queryKey.length === 3) {
+        throw "No valid address provided";
+    }
+    const [,, page, name] = queryKey;
+    const response = await fetch(`${SERVER}/api/model/${page}/${name}`);
+    if (!response.ok) {
+        let errMsg = `error during query for '${page}.${name}'`;
+        try {
+            errMsg = await response.text();
+        } catch {
+        }
+        throw errMsg;
+    }
+    const { diagram }: Model = await response.json();
+    const text = DiagramState.getText(diagram);
+    return text;
+}
+
+function getModelKey(custom: string | undefined): CheckModelKey {
+    if (custom === undefined) {
+        return ["ExampleBlockquote", "Check", "Invalid"];
+    }
+    try {
+        const { page, name } = ElementModelAddress.fromString(custom);
+        return ["ExampleBlockquote", "Check", page, name];
+    } catch {
+        return ["ExampleBlockquote", "Check", "Invalid"];
+    }
+}
+
+function _getHTMLObjectText(object: HTMLObject): string {
+    if (typeof object === "string") {
+        return object;
+    }
+
+    switch (object.type) {
+        case "b":
+        case "i":
+        case "p":
+        case "code":
+            return _getHTMLContentText(object.content);
+    }
+    throw `received unhandled html object type: '${object.type}'`;
+}
+
+function _getHTMLContentText(content: HTMLContent): string {
+    if (content === undefined) {
+        throw "Cannot get text from undefined";
+    }
+    if (Array.isArray(content)) {
+        let output = "";
+        content.forEach((obj) => output += _getHTMLObjectText(obj));
+        return output;
+    }
+    return _getHTMLObjectText(content);
+}
+
+export function createCheckHook(): (blockquote: HTMLBlockquoteObject) => CheckHookState {
+    if (process.env.NODE_ENV !== "development") {
+        return () => { return { type: "ok" }; };
+    }
+    return ({ custom, content }) => {
+        const modelKey = getModelKey(custom);
+        const initialState: InternalState = modelKey.length === 3
+            ? { type: "error", msg: "Invalid custom property received" }
+            : { type: "check address" };
+        const [state, setState] = useState<InternalState>(initialState);
+        const addressQuery = useQuery<Set<string>, string>(
+            ["ExampleBlockquote"],
+            availableQueryFn,
+            {
+                enabled: false,
+                onError: (msg) => setState({ type: "error", msg })
+            }
+        );
+        const textQuery = useQuery<string, string, string, CheckModelKey>(
+            modelKey,
+            textQueryFn,
+            {
+                enabled: false,
+                onError: (msg) => setState({ type: "error", msg })
+            }
+        );
+
+        // run address query
+        useEffect(() => {
+            if (state.type === "check address" && addressQuery.status === "idle") {
+                addressQuery.refetch();
+            }
+        }, [state.type, addressQuery]);
+
+        // run address check
+        useEffect(() => {
+            if (state.type === "check address" && addressQuery.status === "success") {
+                if (modelKey.length === 3) {
+                    throw "error";
+                }
+                const [,, page, name] = modelKey;
+                const addressStr = `${page}.${name}`;
+                const data = addressQuery.data;
+                if (data.has(addressStr)) {
+                    setState({ type: "check model" });
+                } else {
+                    setState({
+                        type: "error",
+                        msg: `No model for '${addressStr}'`
+                    });
+                }
+            }
+        }, [state.type, modelKey, addressQuery.status, addressQuery.data]);
+
+        // run model query
+        useEffect(() => {
+            if (state.type === "check model" && textQuery.status === "idle") {
+                textQuery.refetch();
+            }
+        }, [state.type, textQuery]);
+
+        // run model check
+        useEffect(() => {
+            if (state.type === "check model" && textQuery.status === "success") {
+                const modelText = textQuery.data;
+                try {
+                    const contentText = _getHTMLContentText(content);
+                    if (modelText === contentText) {
+                        setState({ type: "done" });
+                    } else {
+                        setState({
+                            type: "error",
+                            msg: "Discrepancy between model and provided content"
+                        });
+                    }
+                } catch (error) {
+                    const errMsg = typeof error === "string"
+                        ? error
+                        : "Unexpected error occurred";
+                    setState({
+                        type: "error",
+                        msg: errMsg
+                    });
+                }
+            }
+        }, [state.type, textQuery.status, textQuery.data, content]);
+
+        switch (state.type) {
+            case "done":
+                return { type: "ok" };
+            case "check address":
+            case "check model":
+                return { type: "checking" };
+            case "error":
+                return state;
+        }
+    };
+}
