ddfee466b22bd78a27e81dbd9fd0526a26d5a633
ok <oll.korreckt@gmail.com>
2022-06-15 12:10:44 -0400

    Added animation to home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,46 @@
 import { useClientSide } from "@app/utils";
-import { HomePage } from "@utils/home";
-import { NextPage } from "next";
+import { Frame } from "@utils/frame";
+import { FrameError, FrameLoader } from "@utils/frame/io";
+import { HomePage, HomePageProps } from "@utils/home";
+import { GetStaticProps, NextPage } from "next";
+import nodepath from "path";
 import React from "react";
 
-const Home: NextPage = () => {
+const Home: NextPage<HomePageProps> = ({ frames }) => {
     const client = useClientSide();
     return client
-        ? <HomePage/>
+        ? <HomePage frames={frames}/>
         : null;
+};
+
+function createInputFrames(): Frame[] {
+    const sentence = "The quick brown fox jumps over the lazy dog.";
+    const output: Frame[] = [];
+    for (let index = 0; index < sentence.length; index++) {
+        const fragment = sentence.slice(0, index);
+        output.push({
+            duration: 0.1,
+            data: {
+                editMode: "input",
+                inputText: fragment
+            }
+        });
+    }
+    return output;
+}
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+    const loader = FrameLoader.createLoader(nodepath.resolve("src", "data", "frame"));
+    const labelFrames = await loader.getFrames("player");
+    if (FrameError.isFrameError(labelFrames)) {
+        throw `The following error occurred while fetching data: '${labelFrames.msg}'`;
+    }
+    const inputFrames = createInputFrames();
+    return {
+        props: {
+            frames: [...inputFrames, ...labelFrames]
+        }
+    };
 };
 
 export default Home;
--- src/utils/home/HomePage.tsx
+++ src/utils/home/HomePage.tsx
@@ -1,12 +1,25 @@
 import { accessClassName } from "@app/utils";
 import React from "react";
 import styles from "./_styles.module.scss";
+import { EditFormPlayer } from "@app/tricky-components/EditFormPlayer";
+import { Frame } from "@utils/frame";
 
-export const HomePage: React.VFC = () => {
+export interface HomePageProps {
+    frames: Frame[];
+}
+
+export const HomePage: React.VFC<HomePageProps> = ({ frames }) => {
     return (
         <div className={accessClassName(styles, "container")}>
             <div className={accessClassName(styles, "content")}>
-                Hello
+                <h1>Software for diagramming the English language</h1>
+                <p>Am I supposed to use <i>I</i> or <i>me</i>? When do I have to use <i>well</i> instead of <i>good</i>? Do I need to use a comma here or can I leave it alone?</p>
+                <p>Many of us use our intuition when faced with these sorts of questions. In other words, we guess. Fortunately, these sorts of questions are oftentimes governed by formal rules and therefore do have objective answers. However many are deterred by the confusing terminology and the often interdependent rules of grammar.</p>
+                <div className={accessClassName(styles, "player")}>
+                    <EditFormPlayer>
+                        {frames}
+                    </EditFormPlayer>
+                </div>
             </div>
         </div>
     );
