fdf3f4def8f254c94097ec26944e8565cdb7cd1a
ok <oll.korreckt@gmail.com>
2022-06-21 15:14:36 -0400

    Added frame loading functionaity to the EditFormPlayer in order to cut down on the bundle size and initial page load time.

--- src/app/tricky-components/EditFormPlayer/EditFormPlayer.tsx
+++ src/app/tricky-components/EditFormPlayer/EditFormPlayer.tsx
@@ -1,26 +1,102 @@
+import { accessClassName, ControlAnimationContext } from "@app/utils";
 import { Frame } from "@utils/frame";
 import React, { useEffect, useState } from "react";
 import { EditFormFrameRender } from "../EditFormFrameRender";
+import { QueryFunctionContext, useQuery } from "react-query";
+import { SERVER } from "config";
+import { HeapReducer } from "@lib/utils";
+import styles from "./_styles.module.scss";
 
-export interface EditFormPlayerProps {
+type EditFormPlayerKey = ["EditFormPlayer"];
+
+function createInputFrames(): Frame[] {
+    const sentence = "The quick brown fox jumps over the lazy dog.";
+    const output: Frame[] = [{
+        duration: 1,
+        data: {
+            editMode: "input",
+            inputText: ""
+        }
+    }];
+    for (let index = 1; index < sentence.length; index++) {
+        const fragment = sentence.slice(0, index);
+        output.push({
+            duration: 0.08,
+            data: {
+                editMode: "input",
+                inputText: fragment
+            }
+        });
+    }
+    return output;
+}
+
+async function queryFn({}: QueryFunctionContext<EditFormPlayerKey>): Promise<Frame[]> {
+    const queryStr = `${SERVER}/api/frame/player`;
+    const response = await fetch(queryStr);
+    if (!response.ok) {
+        let errorMsg = "error occurred while loading frames for 'player'";
+        try {
+            errorMsg = await response.text();
+        } catch {
+        }
+        throw errorMsg;
+    }
+    const inputFrames = createInputFrames();
+    const labelFrames: Frame[] = await response.json();
+    const output = [...inputFrames, ...labelFrames];
+    return HeapReducer.run(output);
+}
+
+export const EditFormPlayer: React.VFC = () => {
+    const key: EditFormPlayerKey = ["EditFormPlayer"];
+    const query = useQuery(key, queryFn, { retry: false });
+
+    const editFormPlayer = accessClassName(styles, "editFormPlayer");
+
+    switch (query.status) {
+        case "loading":
+            return (
+                <div className={editFormPlayer}>
+                    <div className={accessClassName(styles, "loading")}></div>
+                </div>
+            );
+        case "error":
+            return null;
+        case "success":
+            return (
+                <div className={editFormPlayer}>
+                    <FrameRunner>
+                        {query.data}
+                    </FrameRunner>
+                </div>
+            );
+        default:
+            throw `Invalid status: '${query.status}'`;
+    }
+};
+
+interface FrameRunnerProps {
     children: Frame[];
 }
 
-export const EditFormPlayer: React.VFC<EditFormPlayerProps> = ({ children }) => {
+const FrameRunner: React.VFC<FrameRunnerProps> = ({ children }) => {
     const [index, setIndex] = useState(0);
 
     useEffect(() => {
         const { duration } = children[index];
-        const defDuration = duration ? duration : 1.5;
+        const defDuration = duration ? duration : 2;
         const nextIndex = index === children.length - 1
             ? 0
             : index + 1;
         setTimeout(() => setIndex(nextIndex), defDuration * 1000);
     }, [index, children]);
 
-    const { data } = children[index];
+    const { data, animatingElement } = children[index];
 
     return (
-        <EditFormFrameRender {...data}/>
+        <ControlAnimationContext.Provider value={{ activeElement: animatingElement }}>
+            <EditFormFrameRender {...data}/>
+        </ControlAnimationContext.Provider>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,13 @@
 import { useClientSide } from "@app/utils";
-import { Frame } from "@utils/frame";
-import { FrameError, FrameLoader } from "@utils/frame/io";
-import { HomePage, HomePageProps } from "@utils/home";
-import { GetStaticProps, NextPage } from "next";
-import nodepath from "path";
+import { HomePage } from "@utils/home";
+import { NextPage } from "next";
 import React from "react";
 
-const Home: NextPage<HomePageProps> = ({ frames }) => {
+const Home: NextPage = () => {
     const client = useClientSide();
     return client
-        ? <HomePage frames={frames}/>
+        ? <HomePage/>
         : null;
-};
-
-function createInputFrames(): Frame[] {
-    const sentence = "The quick brown fox jumps over the lazy dog.";
-    const output: Frame[] = [];
-    for (let index = 0; index < sentence.length; index++) {
-        const fragment = sentence.slice(0, index);
-        output.push({
-            duration: 0.08,
-            data: {
-                editMode: "input",
-                inputText: fragment
-            }
-        });
-    }
-    return output;
-}
-
-export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
-    const loader = FrameLoader.createLoader(nodepath.resolve("src", "data", "frame"));
-    const labelFrames = await loader.getFrames("player");
-    if (FrameError.isFrameError(labelFrames)) {
-        throw `The following error occurred while fetching data: '${labelFrames.msg}'`;
-    }
-    const inputFrames = createInputFrames();
-    return {
-        props: {
-            frames: [...inputFrames, ...labelFrames]
-        }
-    };
 };
 
 export default Home;
