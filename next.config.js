9b00f08e7ca585283882922711ce50693b9f6aaf
ok <oll.korreckt@gmail.com>
2022-07-08 12:13:14 -0400

    Added preact to reduce production bundle size

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
+const withPreact = require("next-plugin-preact");
 
-const nextConfig = {
+const nextConfig = withPreact({
     reactStrictMode: true,
     sassOptions: {
         includePaths: [
             path.resolve(__dirname, "src/app")
         ]
     }
-};
+});
 
 module.exports = nextConfig;
