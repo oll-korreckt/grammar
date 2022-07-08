0511a6824607704deb67528f752cb77bf9db5257
ok <oll.korreckt@gmail.com>
2022-07-08 15:06:21 -0400

    Came up with way to prevent console.log calls in production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
+import { useClientSide } from "@app/utils";
 import { AppProps } from "next/app";
 import React from "react";
 import { Layout } from "utils/Layout";
 import "../_globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+    const client = useClientSide();
+    if (client && process.env.NODE_ENV === "production") {
+        console.log = () => { return; };
+    }
+
     return (
         <Layout>
             <Component {...pageProps} />
