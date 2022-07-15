181a46e28a478d6a37dc5d606af8489d11cd680b
ok <oll.korreckt@gmail.com>
2022-07-15 10:53:03 -0400

    Removed preact from project as it seemed to be causing memory leaks

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
-const withPreact = require("next-plugin-preact");
 
-const nextConfig = withPreact({
+const nextConfig = {
     reactStrictMode: true,
     sassOptions: {
         includePaths: [
             path.resolve(__dirname, "src/app")
         ]
     }
-});
+};
 
 module.exports = nextConfig;
