d70e1e6fb0399924a67a957b3a6137a7cda18e0b
ok <oll.korreckt@gmail.com>
2022-07-20 15:44:16 -0400

    Using a directory that was taken from an example given by Vercel. We'll see if that works.

--- src/pages/api/model/index.ts
+++ src/pages/api/model/index.ts
@@ -3,7 +3,7 @@ import { isModelLoaderError, ModelLoader } from "@utils/model/io";
 import { NextApiRequest, NextApiResponse } from "next";
 import nodepath from "path";
 
-const loader = ModelLoader.createLoader(nodepath.join(process.cwd(), "public", "data", "element"));
+const loader = ModelLoader.createLoader(nodepath.join(process.cwd(), "json", "element"));
 
 async function handleGet(req: NextApiRequest, res: NextApiResponse): Promise<void> {
     const output = await loader.getModelAddresses();
