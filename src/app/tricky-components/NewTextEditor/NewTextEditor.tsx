4cc49d2fb1a6dd139f790bc21645e9ab506cb18a
ok <oll.korreckt@gmail.com>
2022-07-20 14:14:33 -0400

    Seem to have gotten NewInputForm working

--- src/app/tricky-components/NewTextEditor/NewTextEditor.tsx
+++ src/app/tricky-components/NewTextEditor/NewTextEditor.tsx
@@ -1,25 +1,55 @@
 import { accessClassName } from "@app/utils";
-import React from "react";
+import { makeRefComponent, mergeRefs } from "@app/utils/hoc";
+import React, { useEffect, useRef } from "react";
 import styles from "./_styles.module.scss";
+import hash from "object-hash";
 
 export interface NewTextEditorProps {
     children?: string;
     onInputChange?: (input: string) => void;
     errors?: boolean;
+    cursorPosition?: [line: number, column: number];
 }
 
-export const NewTextEditor: React.VFC<NewTextEditorProps> = ({ onInputChange, errors, children }) => {
-    const classes = [
-        "textEditor"
-    ];
+function getPosition(text: string, [line, column]: [number, number]): number {
+    const textLines = text.split("\n");
+    let output = 0;
+    for (let index = 0; index < line; index++) {
+        const textLine = textLines[index];
+        output += textLine.length;
+    }
+    output += column;
+    return output;
+}
+
+export const NewTextEditor = makeRefComponent<HTMLTextAreaElement, NewTextEditorProps>("NewTextEditor", ({ onInputChange, errors, cursorPosition, children }, passRef) => {
+    const ref = useRef<HTMLTextAreaElement>(null);
+    const classes = ["textEditor"];
     if (errors) {
         classes.push("textEditorError");
     }
+
+    const cursorPositionHash = hash(cursorPosition ? cursorPosition : null);
+
+    useEffect(() => {
+        if (ref.current === null
+            || ref.current.textContent === null
+            || cursorPosition === undefined) {
+            return;
+        }
+        const pos = getPosition(ref.current.textContent, cursorPosition);
+        ref.current.focus();
+        ref.current.selectionStart = pos;
+        ref.current.selectionEnd = pos;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [cursorPositionHash]);
+
     return (
         <textarea
+            ref={mergeRefs(passRef, ref)}
             className={accessClassName(styles, ...classes)}
             onChange={(e) => onInputChange && onInputChange(e.target.value)}
             value={children}
         />
     );
-};
+});
