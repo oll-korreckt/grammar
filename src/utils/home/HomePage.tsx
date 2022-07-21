aebbf0fc1f4eab4b89304441b0ee228480a2029d
ok <oll.korreckt@gmail.com>
2022-07-21 09:59:41 -0400

    Minor changes

--- src/utils/home/HomePage.tsx
+++ src/utils/home/HomePage.tsx
@@ -11,7 +11,7 @@ export const HomePage: React.VFC = () => {
     return (
         <div className={accessClassName(styles, "container")}>
             <h1>Software for diagramming the English language</h1>
-            <p>Checking preview. Am I supposed to use <i>I</i> or <i>me</i>? When do I have to use <i>well</i> instead of <i>good</i>? Do I need to use a comma here or can I leave it alone?</p>
+            <p>Am I supposed to use <i>I</i> or <i>me?</i> When do I have to use <i>well</i> instead of <i>good?</i> Do I need to use a comma here or can I leave it alone?</p>
             <p>Many of us use our intuition when faced with these sorts of questions. In other words, we guess. Fortunately, these sorts of questions are oftentimes governed by formal rules and therefore do have objective answers. However many are deterred by the confusing terminology and the often interdependent rules of grammar.</p>
             <div
                 className={accessClassName(styles, "launchContainer")}
